6048ac71f2c86d5bfb12253a7139b105fdb7eb22
Avior <florian.bouillon@delta-wings.net>
2020-03-16 21:21:46 +0100

    Fixed DB
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

--- cards/xy/xy5/135.ts
+++ cards/xy/xy5/135.ts
@@ -44,12 +44,9 @@ const card: Card = {
 
 
 
-	attacks: [{
-		name: {},
-		text: {
-			fr: "Choisissez l'un de vos Pokémon de base en jeu. Si vous avez dans votre main une carte de Niveau 2 qui est une évolution du Pokémon choisi, placez-la sur le Pokémon de base. (Cela équivaut à faire évoluer le Pokémon.) Vous ne pouvez pas utiliser cette carte pendant votre premier tour ni sur un Pokémon de base qui a été joué pendant ce tour.",
-		},
-	}],
+	effect: {
+		fr: "Choisissez l'un de vos Pokémon de base en jeu. Si vous avez dans votre main une carte de Niveau 2 qui est une évolution du Pokémon choisi, placez-la sur le Pokémon de base. (Cela équivaut à faire évoluer le Pokémon.) Vous ne pouvez pas utiliser cette carte pendant votre premier tour ni sur un Pokémon de base qui a été joué pendant ce tour.",
+	},
 
 
 
