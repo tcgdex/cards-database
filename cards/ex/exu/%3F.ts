13e1ffbbe4b5276b439437d9c162ae83e10c3152
Avior <florian.bouillon@delta-wings.net>
2021-01-31 12:40:41 +0100

    Created a new version of the ?.ts file

---
+++ cards/ex/exu/%3F.ts
@@ -0,0 +1,86 @@
+import Card from '@tcgdex/sdk/interfaces/Card'
+import Type from '@tcgdex/sdk/interfaces/Type'
+import Tag from '@tcgdex/sdk/interfaces/Tag'
+import Rarity from '@tcgdex/sdk/interfaces/Rarity'
+import AbilityType from '@tcgdex/sdk/interfaces/AbilityType'
+import Category from '@tcgdex/sdk/interfaces/Category'
+
+const card: Card = {
+
+	// ids
+	id: "exu-?",
+	localId: "?",
+
+	// Card informations
+	name: {
+		en: "Unown",
+	},
+
+	hp: 60,
+
+	type: [
+		Type.PSYCHIC,
+	],
+
+	dexId: 201,
+
+	image: {
+		low: {
+			en: "https://assets.tcgdex.net/en/ex/exu/?/low",
+		},
+		high: {
+			en: "https://assets.tcgdex.net/en/ex/exu/?/high",
+		},
+	},
+
+	evolveFrom: {},
+
+	tags: [
+		Tag.BASIC,
+	],
+
+	illustrator: "Kyoko Koizumi",
+
+	abilities: [{
+		id: 88,
+		type: AbilityType.POKEPOWER,
+		name: {
+			en: "Shuffle",
+		},
+		text: {
+			en: "Once during your turn (before your attack), you may search your deck for another Unown and switch it with Unown. (Any cards attached to Unown, damage counters, Special Conditions, and effects on it are now on the new Pokémon.) If you do, put Unown on top of your deck. Shuffle your deck afterward. You can't use more than 1 Shuffle Poké-Power each turn.",
+		}
+	}],
+
+	attacks: [{
+		cost: [
+			Type.COLORLESS
+		],
+		name: {
+			en: "Hidden Power",
+		},
+		text: {
+			en: "Choose a card from your hand and put it face down. Your opponent guesses if that card is a Pokémon, Trainer, or Energy card. Reveal the card. If your opponent guessed wrong, draw 2 cards. Put the card back into your hand.",
+		},
+	}],
+
+	weaknesses: [{
+		type: Type.PSYCHIC,
+		value: "×2"
+	}],
+
+
+
+
+
+	rarity: Rarity.RARE,
+
+	category: Category.POKEMON,
+
+	set: {
+		name: "Unseen Forces",
+		code: "exu"
+	}
+}
+
+export default card
