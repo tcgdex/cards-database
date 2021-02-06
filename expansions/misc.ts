2d31b6c403562aadda6bfe7c28419a7b29d9f273
Avior <florian.bouillon@delta-wings.net>
2021-02-06 01:50:05 +0100

    Updated SWSH informations
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

---
+++ expansions/misc.ts
@@ -0,0 +1,11 @@
+import Expansion from "@tcgdex/sdk/interfaces/Expansion";
+
+const exp: Expansion = {
+	name: {
+		en: "Misc",
+		fr: "Autre"
+	},
+	code: "misc"
+}
+
+export default exp
--- sets/swsh/swshp.ts
+++ sets/swsh/swshp.ts
@@ -2,28 +2,22 @@ import Set from '@tcgdex/sdk/interfaces/Set'
 import swsh from '../../expansions/swsh'
 
 const set: Set = {
-	name: {
-		en: "Promos",
-		fr: "Promos"
-	},
+	name: "Promos",
 
 	expansion: swsh,
 
 	code: "swshp",
-	// tcgoCode
 
 	cardCount: {
-		total: 13,
-		official: 13
+		total: 107,
+		official: 107
 	},
-
-	format: "SWSH[000]",
 
 	releaseDate: "2019-11-15",
 
 	legal: {
-		standard: true,
-		expanded: true
+		standard: false,
+		expanded: false
 	}
 }
 
