b8dd3fe1b72142f3140539e09f15922a50970d69
Florian BOUILLON <florian.bouillon@delta-wings.net>
2021-04-28 11:49:08 +0200

    Added current set informations
    Signed-off-by: Florian BOUILLON <florian.bouillon@delta-wings.net>

---
+++ data/Sword & Shield/Chilling Reign.ts
@@ -0,0 +1,27 @@
+import { Set } from '../../interfaces'
+import serie from '../Sword & Shield'
+
+const swsh3: Set = {
+	id: "swsh5",
+	name: {
+		en: "Chilling Reign",
+		fr: "Règne de Glace"
+,
+	},
+	serie: serie,
+
+
+	cardCount: { // NOT confirmed
+		total: 140,
+		official: 140
+	},
+
+	releaseDate: "2021-06-18",
+
+	legal: {
+		standard: true,
+		expanded: true
+	},
+}
+
+export default swsh3
