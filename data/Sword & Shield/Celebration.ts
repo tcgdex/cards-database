60d020a0c55c1f87a1cdd88e65ebd2183e26c592
Avior <florian.bouillon@delta-wings.net>
2021-09-21 00:14:32 +0200

    Added Celebration set informations
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

---
+++ data/Sword & Shield/Celebration.ts
@@ -0,0 +1,20 @@
+import { Set } from '../../interfaces'
+import serie from '../Sword & Shield'
+
+const set: Set = {
+	id: "swsh7.5",
+
+	name: {
+		en: "Celebration"
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 0 // not known
+	},
+
+	releaseDate: "2021-01-08"
+}
+
+export default set
