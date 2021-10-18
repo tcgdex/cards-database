bf27f42720268b757fd174130475958cca8b7173
Avior <github@avior.me>
2021-10-18 11:56:52 +0200

    Added Pokémon Futsal 2020 (#107)
    Signed-off-by: Avior <github@avior.me>

---
+++ data/Sword & Shield/Pokémon Futsal 2020.ts
@@ -0,0 +1,20 @@
+import { Set } from '../../interfaces'
+import serie from '../Sword & Shield'
+
+const swshp: Set = {
+	id: "fut2020",
+
+	name: {
+		en: "Pokémon Futsal 2020"
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 5
+	},
+
+	releaseDate: "2020-09-11"
+}
+
+export default swshp
