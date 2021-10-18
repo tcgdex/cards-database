70e03d9c015eff0ce7bc9aca87f231d950423bcc
Avior <github@avior.me>
2021-10-18 11:39:22 +0200

    Added cards
    Signed-off-by: Avior <github@avior.me>

---
+++ data/E-Card/Best of game/6.ts
@@ -0,0 +1,41 @@
+import { Card } from '../../../interfaces'
+import Set from '../Best of game'
+
+const card: Card = {
+	set: Set,
+
+	name: {
+		en: "Dark Ivysaur"
+	},
+
+	illustrator: "Shin-ichi Yoshida",
+	rarity: "None",
+	category: "Pokemon",
+	stage: "Stage1",
+
+	abilities: [{
+		type: "Poke-BODY",
+
+		name: {
+			en: "Vine Pull"
+		},
+
+		effect: {
+			en: "Once during your turn when Dark Ivysaur retreats, choose 1 of your opponent’s Benched Pokémon and switch it with his or her Active Pokémon."
+		}
+	}],
+
+	attacks: [{
+		cost: ["Grass", "Grass"],
+
+		name: {
+			en: "Fury Strikes"
+		},
+
+		effect: {
+			en: "Your opponent puts 3 markers onto his or her Pokémon (divided as he or she chooses). (More than 1 marker can be put on the same Pokémon.) Then, this attack does 10 damage to each Pokémon for each marker on it. Don’t apply Weakness and Resistance. Remove the markers at the end of the turn."
+		}
+	}]
+}
+
+export default card
