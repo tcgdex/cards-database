b365c1555923ecb764d35b51c0d720cfe6837b08
Avior <github@avior.me>
2021-11-22 15:08:42 +0100

    feat: Add Poké Card Creator Pack cards (#184)
    Signed-off-by: Avior <github@avior.me>

---
+++ data/EX/Poké Card Creator Pack/5.ts
@@ -0,0 +1,61 @@
+import { Card } from '../../../interfaces'
+import Set from '../Poké Card Creator Pack'
+
+const card: Card = {
+	name: {
+		en: "Pikachu"
+	},
+	illustrator: "Sylvia Forrest",
+	rarity: "None",
+	category: "Pokemon",
+
+	set: Set,
+	dexId: [
+		25,
+	],
+	hp: 60,
+	types: [
+		"Lightning",
+	],
+	stage: "Basic",
+	retreat: 1,
+
+
+	attacks: [
+		{
+			cost: [
+				"Colorless",
+			],
+			name: {
+				en: "Thundershock"
+			},
+			effect: {
+				en: "Flip a coin. If heads, the Defending Pokémon is now Paralyzed."
+			},
+			damage: 10,
+
+		},
+		{
+			cost: [
+				"Lightning",
+				"Lightning",
+				"Colorless",
+			],
+			name: {
+				en: "Hypno Splash"
+			},
+			effect: {
+				en: "Discard all Lightning Energy cards attached to Pikachu or this attack does nothing."
+			},
+			damage: 60,
+
+		}
+	],
+	weaknesses: [
+		{
+			type: "Fighting"
+		},
+	]
+}
+
+export default card
