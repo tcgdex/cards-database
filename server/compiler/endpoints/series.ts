b47642453bb4d678b460d94f1c7e96bb0a1034ee
Avior <florian.bouillon@delta-wings.net>
2021-11-23 09:51:49 +0100

    refactor: Simplified compiler files generators
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

--- server/compiler/endpoints/series.ts
+++ server/compiler/endpoints/series.ts
@@ -1,32 +1,10 @@
-import { Serie as SerieSingle, SerieResume } from '../../../meta/definitions/api'
-import { Languages, Serie } from '../../../interfaces'
-import { Endpoint } from '../compilerInterfaces'
-import { getSeries, serieToSerieSimple, serieToSerieSingle } from '../utils/serieUtil'
+import { SupportedLanguages } from '../../../interfaces'
+import { FileFunction } from '../compilerInterfaces'
+import { getSeries, serieToSerieSingle } from '../utils/serieUtil'
 
-type SerieList = Array<SerieResume>
-
-export default class implements Endpoint<SerieList, SerieSingle, Record<string, any>, Array<Serie>> {
-
-	public constructor(
-		private lang: keyof Languages
-	) {}
-
-	public async index(common: Array<Serie>): Promise<Array<SerieResume>> {
-		return Promise.all(common.map((s) => serieToSerieSimple(s, this.lang)))
-	}
-
-	public async item(common: Array<Serie>): Promise<Record<string, SerieSingle>> {
-		const items: Record<string, SerieSingle> = {}
-		for await (const val of common) {
-			const gen = await serieToSerieSingle(val, this.lang)
-			const name = val.name[this.lang] as string
-			items[val.id] = gen
-		}
-		return items
-	}
-
-	public async common(): Promise<Array<Serie>> {
-		return getSeries(this.lang)
-	}
-
+const fn: FileFunction = async (lang: SupportedLanguages) => {
+	const common = await getSeries(lang)
+	return await Promise.all(common.map((val) => serieToSerieSingle(val, lang)))
 }
+
+export default fn
