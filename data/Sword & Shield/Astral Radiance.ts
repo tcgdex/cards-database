ba3c156c17aca5e9ed6e94eb8c484f631f8625be
Avior <github@avior.me>
2022-05-26 19:14:45 +0200

    fix: Non-english names
    Signed-off-by: Avior <github@avior.me>

--- data/Sword & Shield/Astral Radiance.ts
+++ data/Sword & Shield/Astral Radiance.ts
@@ -5,7 +5,12 @@ const set: Set = {
 	id: "swsh10",
 
 	name: {
-		en: "Astral Radiance"
+		en: "Astral Radiance",
+		fr: "Astres Radieux",
+		es: "Resplandor Astral",
+		it: "Lucentezza Siderale",
+		de: "Astralglanz",
+		pt: "Estrelas Radiantes"
 	},
 
 	serie: serie,
