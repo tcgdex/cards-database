8192542c1d236e7b7ad016ff93099122e0e87369
Avior <f.bouillon@aptatio.com>
2022-12-09 14:42:22 +0100

    feat(server): Add better logging to detect potential errors
    Signed-off-by: Avior <f.bouillon@aptatio.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,33 @@
 import express from 'express'
-import graphql from './V2/graphql'
-import jsonEndpoints from './V2/endpoints/jsonEndpoints'
 import status from './status'
+import jsonEndpoints from './V2/endpoints/jsonEndpoints'
+import graphql from './V2/graphql'
 // Current API version
 const VERSION = 2
 
 // Init Express server
 const server = express()
 
-// Set global headers
+// Set CORS global headers
 server.use((_, res, next) => {
 	res
 		.setHeader('Access-Control-Allow-Origin', '*')
 		.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
 		.setHeader('Access-Control-Allow-Headers', 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range')
 		.setHeader('Access-Control-Expose-Headers', 'Content-Length,Content-Range')
+	next()
+})
+
+// Route logging / Error logging for debugging
+server.use((req, res, next) => {
+	res.on('close', () => {
+		console.log(`[${new Date().toISOString()}]: ${req.method.padStart(7, ' ')} ${res.statusCode} ${(req.baseUrl ?? '') + req.url}`)
+	})
+	res.on('error', (err) => {
+		console.error('Error:')
+		console.error(err)
+	})
+
 	next()
 })
 
