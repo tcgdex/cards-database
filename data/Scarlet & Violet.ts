6e18c52b16516d773db955eb7fb37c195c02c28f
Avior <github@avior.me>
2023-03-31 10:55:11 +0200

    feat: Add serie and set infos
    Signed-off-by: Avior <github@avior.me>

---
+++ data/Scarlet & Violet.ts
@@ -0,0 +1,22 @@
+import { Serie } from '../interfaces'
+
+const serie: Serie = {
+	id: "sv",
+	name: {
+		de: "Karmesin & Purpur",
+		en: "Scarlet & Violet",
+		es: "Escarlata y Púrpura",
+		fr: "Écarlate et Violet",
+		it: "Scarlatto e Violetto",
+		pt: "Escarlate e Violeta"
+	},
+
+	energies: [
+		'Darkness', 'Dragon',
+		'Fighting', 'Fire', 'Grass',
+		'Lightning', 'Metal',
+		'Psychic', 'Water'
+	]
+}
+
+export default serie
