15c400e656830b3991633e760a2bcd5f465bc737
Avior <github@avior.me>
2023-12-27 22:42:08 +0100

    fix: undaunted rarities
    Signed-off-by: Avior <github@avior.me>

--- data/HeartGold & SoulSilver/Undaunted/7.ts
+++ data/HeartGold & SoulSilver/Undaunted/7.ts
@@ -9,7 +9,7 @@ const card: Card = {
 	},
 
 	illustrator: "Kent Kanetsuna/Direc. Shinji Higuchi",
-	rarity: "Rare",
+	rarity: "Rare Holo",
 	category: "Pokemon",
 	set: Set,
 
--- data/HeartGold & SoulSilver/Undaunted/9.ts
+++ data/HeartGold & SoulSilver/Undaunted/9.ts
@@ -9,7 +9,7 @@ const card: Card = {
 	},
 
 	illustrator: "Atsuko Nishida",
-	rarity: "Rare",
+	rarity: "Rare Holo",
 	category: "Pokemon",
 	set: Set,
 
