17ae969792e445ad0252604d7780c0021b282dbd
Avior <github@avior.me>
2024-05-10 04:36:20 +0200

    fix: Invalid stats displayed
    Signed-off-by: Avior <github@avior.me>

--- server/compiler/endpoints/stats.ts
+++ server/compiler/endpoints/stats.ts
@@ -1,8 +1,8 @@
-import { getSets, setToSetSingle } from '../utils/setUtil'
 import { SupportedLanguages } from '../../../interfaces'
 import { FileFunction } from '../compilerInterfaces'
 import { getCards } from '../utils/cardUtil'
 import { getSeries } from '../utils/serieUtil'
+import { getSets, setToSetSingle } from '../utils/setUtil'
 
 interface Stats {
 	count: number
@@ -14,9 +14,11 @@ interface Stats {
 const fn: FileFunction = async (lang: SupportedLanguages) => {
 	const stats: Partial<Stats> = {}
 	stats.count = (await getCards(lang)).length
+	// temporary fix until a better solution is found
+	const referenceLang = ['ja', 'ko', 'zh-tw', 'id', 'th', 'zh-cn'].includes(lang) ? 'ja' : 'en'
 
 	const langSets = await Promise.all(await getSets(undefined, lang).then((sets) => sets.map(async (set) => await setToSetSingle(set, lang))))
-	const englishSets = await Promise.all(await getSets(undefined, 'en').then((sets) => sets.map(async (set) => await setToSetSingle(set, 'en'))))
+	const englishSets = await Promise.all(await getSets(undefined, referenceLang).then((sets) => sets.map(async (set) => await setToSetSingle(set, referenceLang))))
 	stats.total = langSets.reduce((p, set) => p + (englishSets.find((s) => set.id === s.id)?.cardCount?.total ?? 0), 0)
 	stats.images = langSets.reduce((p1, set) => p1 + (set.cards.reduce((p2, card) => p2 + (card.image ? 1 : 0), 0)), 0)
 	stats.sets = {}
