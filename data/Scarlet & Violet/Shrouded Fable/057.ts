3b02e9be818b2e8fdf7290e6cb3ffdda110e29b9
Duncan <6261352+dsnidr@users.noreply.github.com>
2024-12-01 17:25:10 -0500

    fix: Add SV06.5 Shrouded Fable card variants (#598)

--- data/Scarlet & Violet/Shrouded Fable/057.ts
+++ data/Scarlet & Violet/Shrouded Fable/057.ts
@@ -26,7 +26,11 @@ const card: Card = {
 	},
 
 	trainerType: "Supporter",
-	regulationMark: "H"
+	regulationMark: "H",
+
+	variants: {
+		holo: false
+	}
 }
 
 export default card
--- data/Scarlet & Violet/Shrouded Fable/060.ts
+++ data/Scarlet & Violet/Shrouded Fable/060.ts
@@ -26,7 +26,12 @@ const card: Card = {
 	},
 
 	trainerType: "Stadium",
-	regulationMark: "H"
+	regulationMark: "H",
+
+	variants: {
+		normal: false,
+		reverse: false
+	}
 }
 
 export default card
