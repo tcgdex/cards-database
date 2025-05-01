fc3e499e7b138c40bd44811424565fac4caa4433
Avior <git@avior.me>
2025-05-01 23:22:37 +0200

    fix: dex-id url not returning valid cards & filtering not working using `dexId`

--- server/src/V2/endpoints/jsonEndpoints.ts
+++ server/src/V2/endpoints/jsonEndpoints.ts
@@ -247,6 +247,15 @@ server
 					result = Serie.findOne(lang, { name: id })?.full()
 				}
 				break
+			case 'dex-ids': {
+				result = {
+					name: parseInt(id, 10),
+					// @ts-expect-error current behavior is normal
+					cards: Card.find(lang, { dexId: { $eq: parseInt(id, 10) }})
+						.map((c) => c.resume())
+				}
+				break
+			}
 			default:
 				if (!endpointToField[endpoint]) {
 					break
