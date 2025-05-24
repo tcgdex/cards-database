447e9e81786529e7b31082a1e2f7221e5bcbbf0b
Avior <git@avior.me>
2025-05-24 20:51:28 +0200

    feat: Add sv11? set files
    Signed-off-by: Avior <git@avior.me>

---
+++ data/Scarlet & Violet/Black Bolt.ts
@@ -0,0 +1,30 @@
+import { Set } from '../../interfaces'
+import serie from '../Scarlet & Violet'
+
+const set: Set = {
+	id: "sv11B", // not sure
+
+	name: {
+		// de: "Ewige Rivalen",
+		en: "Black Bolt",
+		// es: "Rivales Predestinados",
+		// fr: "Rivalités Destinées",
+		// it: "Rivali Predestinati",
+		// pt: "Rivais Predestinados",
+		// 'es-mx': 'Rivales Predestinados'
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 170
+	},
+
+	releaseDate: "2023-07-18",
+
+	abbreviations: {
+		official: "BLK"
+	}
+}
+
+export default set
