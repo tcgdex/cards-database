890e5b36cde961a22fc7074393bcbe2700474af8
Avior <git@avior.me>
2025-05-24 20:54:32 +0200

    feat: Add A3a set file
    Signed-off-by: Avior <git@avior.me>

---
+++ data/Pokémon TCG Pocket/Extradimensional Crisis.ts
@@ -0,0 +1,44 @@
+import { Set } from '../../interfaces'
+import serie from '../Pokémon TCG Pocket'
+
+const set: Set = {
+	id: "A3a",
+
+	name: {
+		// de: "Unschlagbare Gene",
+		en: "Extradimensional Crisis",
+		// es: "Genes Formidables",
+		// fr: "Puissance Génétique",
+		// it: "Geni Supremi",
+		// 'pt-br': "Dominação Genética",
+		// ko: '최강의 유전자'
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 999 // ???
+	},
+
+	// boosters: {
+	// 	mewtwo: {
+	// 		name: {
+	// 			en: 'Mewtwo'
+	// 		}
+	// 	},
+	// 	charizard: {
+	// 		name: {
+	// 			en: 'Charizard'
+	// 		}
+	// 	},
+	// 	pikachu: {
+	// 		name: {
+	// 			en: 'Pikachu'
+	// 		}
+	// 	}
+	// },
+
+	releaseDate: "2025-05-29"
+}
+
+export default set
