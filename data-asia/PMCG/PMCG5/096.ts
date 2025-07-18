860fac2dc3805420459b72d20a03515cb5f5873a
thomas-bassett <thomas.bassett1@sainsburys.co.uk>
2025-07-18 12:54:21 +0100

    adjust some cards

--- data-asia/PMCG/PMCG5/096.ts
+++ data-asia/PMCG/PMCG5/096.ts
@@ -19,11 +19,6 @@ const card: Card = {
       variants: [
         {
           type: "holo",
-          subtype: "unlimited",
-        },
-        {
-          type: "holo",
-          stamp: ["1st edition"],
         },
       ],
 };
