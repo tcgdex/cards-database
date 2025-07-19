49b5c67d89edbb35429109964d90fdff38100430
thomas-bassett <thomas.bassett1@sainsburys.co.uk>
2025-07-19 08:51:45 +0100

    adjust cards

--- data-asia/PCG/PCG5/058.ts
+++ data-asia/PCG/PCG5/058.ts
@@ -93,10 +93,10 @@ const card: Card = {
 
       variants: [
         {
-          type: "normal",
+          type: "holo",
         },
         {
-          type: "normal",
+          type: "holo",
           stamp: ["1st edition"],
         },
       ],
