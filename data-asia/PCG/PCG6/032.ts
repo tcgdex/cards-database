0b94cd62b679762af44020495495159426c6c5d5
thomas-bassett <thomas.bassett1@sainsburys.co.uk>
2025-07-19 09:17:02 +0100

    adjust cards

--- data-asia/PCG/PCG6/032.ts
+++ data-asia/PCG/PCG6/032.ts
@@ -48,11 +48,11 @@ const card: Card = {
 
       variants: [
         {
-          type: "normal",
+          type: "holo",
           subtype: "unlimited',
         },
         {
-          type: "normal",
+          type: "holo",
           stamp: ["1st edition"],
         },
       ],
--- data-asia/PCG/PCG6/041.ts
+++ data-asia/PCG/PCG6/041.ts
@@ -93,11 +93,11 @@ const card: Card = {
 
       variants: [
         {
-          type: "normal",
+          type: "holo",
           subtype: "unlimited',
         },
         {
-          type: "normal",
+          type: "holo",
           stamp: ["1st edition"],
         },
       ],
