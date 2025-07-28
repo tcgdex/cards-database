2d8aaa8fff0504cc757ee708cee06c14a8b6d404
adresa97 <adriresa97@gmail.com>
2025-07-28 12:58:54 +0200

    fix: Add spanish translation to Pocket promo card 74

--- data/Pokémon TCG Pocket/Promos-A/074.ts
+++ data/Pokémon TCG Pocket/Promos-A/074.ts
@@ -17,7 +17,7 @@ const card: Card = {
 
 	description: {
 		en: "It electrifies its claws and tears its opponents\napart with them. Even if they dodge its attack,\nthey'll be electrocuted by the flying sparks.",
-		//es: "Se comunican con sus compañeros chocando los\npicos. El número de veces y la fuerza con la que\nlo hacen transmiten sus distintos sentimientos."
+		es: "Hace jirones al oponente con sus garras electrificadas. Aunque\neste esquive los golpes, acaba electrocutado por las descargas."
 	},
 
 	stage: "Basic",
@@ -27,19 +27,19 @@ const card: Card = {
 
 		name: {
 			en: "Thunderclap Flash",
-			//es: 
+			es: "Destello Fulgurante"
 		},
 
 		effect: {
-			en: "At the end of your first turn, take a {L} Energy from your Energy Zone and attach it to this Pokémon."
-			//es: 
+			en: "At the end of your first turn, take a {L} Energy from your Energy Zone and attach it to this Pokémon.",
+			es: "Al final de tu primer turno, se unirá 1 Energía {L} de tu área de Energía a este Pokémon."
 		}
 	}],
 
 	attacks: [{
 		name: {
-			en: "Lightning Claw"
-			//es: 
+			en: "Lightning Claw",
+			es: "Garra Relámpago" 
 		},
 
 		damage: 50,
