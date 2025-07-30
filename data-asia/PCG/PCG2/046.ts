47e7b6b25a99e7c5c7b25e783fd04fdec929be7d
thomas-bassett <thomasbassett94@gmail.com>
2025-07-30 12:31:10 +0100

    remove langs

--- data-asia/PCG/PCG2/046.ts
+++ data-asia/PCG/PCG2/046.ts
@@ -4,14 +4,8 @@ import Set from "../PCG2"
 const card: Card = {
       set: Set,
       name: {
-         en: "Deoxys ex",
-         ja: "Deoxys ex",
-         fr: "Deoxys ex",
-         de: "Deoxys ex",
-         es: "DEOXYS EX",
-         it: "Deoxys Ex",
-         pt: "DEOXYS EX",
-      },
+                  ja: "Deoxys ex",
+                                                   },
 
       rarity: "Holo Rare",
       category: "Pokemon",
@@ -23,46 +17,22 @@ const card: Card = {
       abilities: [
         {
           name: {
-            en: "Form Change",
-            ja: "フォームの変更",
-            fr: "Changement de forme",
-            de: "Formänderung",
-            es: "Cambio de forma",
-            it: "Modifica del modulo",
-            pt: "Mudança de forma",
-          },
+                        ja: "フォームの変更",
+                                                                      },
           effect: {
-            en: "Once during your turn (before your attack), you may search your deck for another Deoxys ex and switch it with Deoxys ex. (Any cards attached to Deoxys ex, damage counters, Special Conditions, and effects on it are now on the new PokÃ©mon.) If you do, put Deoxys ex on top of your deck. Shuffle your deck afterward. You can't use more than 1 Form Change PokÃ©-Power each turn.",
-            ja: "ターン中に（攻撃の前）、デッキを検索して別のDeoxys Exを検索し、Deoxys Exで切り替えることができます。 （Deoxys Ex、ダメージカウンター、特別な条件、およびそれに対する影響に添付されたカードは、現在新しいPokã©Mon。）にある場合は、Deoxys Exをデッキの上に置きます。その後、デッキをシャッフルします。ターンごとに1つ以上のフォーム変更Pokã©-Powerを使用することはできません。",
-            fr: "Une fois pendant votre tour (avant votre attaque), vous pouvez rechercher votre deck un autre Deoxys ex et le changer avec Deoxys ex. (Toutes les cartes attachées à Deoxys ex, les compteurs de dégâts, les conditions spéciales et les effets à ce sujet sont maintenant sur le nouveau Pokã © Mon.) Si vous le faites, mettez Deoxys Ex sur votre jeu. Mélanger votre deck par la suite. Vous ne pouvez pas utiliser plus d'un changement de formulaire Pokã © -Power à chaque tour.",
-            de: "Sobald Sie während Ihres Zuges (vor Ihrem Angriff) sind, können Sie Ihr Deck nach einem anderen Desoxys Ex durchsuchen und es mit Deoxys Ex wechseln. (Alle Karten, die an Deoxys Ex, Schadenszähler, besondere Bedingungen und Auswirkungen auf die neuen Poké Mons sind.) Wenn Sie dies tun.) Wenn Sie dies tun, legen Sie Deoxys Ex auf Ihr Deck. Mischen Sie anschließend Ihr Deck. Sie können nicht mehr als 1 Formänderung Poké-Kraft in jeder Runde verwenden.",
-            es: "Una vez durante su turno (antes de su ataque), puede buscar en su mazo otro Deoxys Ex y cambiarlo con Deoxys Ex. (Cualquier tarjetas adjuntas a Deoxys EX, contadores de daño, condiciones especiales y efectos en ellas ahora están en el nuevo Poké Mon.) Si lo hace, coloque Deoxys Ex sobre su mazo. Baraja tu mazo después. No puede usar más de 1 formulario de cambio de forma en cada vuelta.",
-            it: "Una volta durante il tuo turno (prima del tuo attacco), puoi cercare nel tuo mazzo un altro Deoxys Ex e cambiarlo con Deoxys Ex. (Eventuali carte collegate a Deoxys Ex, contatori di danni, condizioni speciali ed effetti su di esso sono ora sul nuovo poké mon.) Se lo fai, metti Deoxys ex sopra il mazzo. Shuffle il tuo mazzo in seguito. Non è possibile utilizzare più di 1 modifica del modulo Poké-power ogni turno.",
-            pt: "Uma vez durante o seu turno (antes do seu ataque), você pode pesquisar no seu baralho por outro Deoxys ex e trocá -lo com Deoxys Ex. (Quaisquer cartões anexados a Deoxys Ex, contadores de danos, condições especiais e efeitos nela estão agora no novo Poké Mon.) Se o fizer, coloque Deoxys EX em cima do seu baralho. Afaste seu baralho depois. Você não pode usar mais de 1 forma de alteração de Poké-Power a cada turno.",
-          },
+                        ja: "ターン中に（攻撃の前）、デッキを検索して別のDeoxys Exを検索し、Deoxys Exで切り替えることができます。 （Deoxys Ex、ダメージカウンター、特別な条件、およびそれに対する影響に添付されたカードは、現在新しいPokã©Mon。）にある場合は、Deoxys Exをデッキの上に置きます。その後、デッキをシャッフルします。ターンごとに1つ以上のフォーム変更Pokã©-Powerを使用することはできません。",
+                                                                      },
       }],
 
       attacks: [
         {
           cost: ["Psychic", "Colorless", "Colorless"],
           name: {
-            en: "Psychic Shield",
-            ja: "サイキックシールド",
-            fr: "Bouclier psychique",
-            de: "Psychischer Schild",
-            es: "Escudo psíquico",
-            it: "Scudo psichico",
-            pt: "Escudo psíquico",
-          },
+                        ja: "サイキックシールド",
+                                                                      },
           effect: {
-            en: "Prevent all effects of attacks, including damage, done to Deoxys ex by your opponent's Pokemon-ex during your opponent's next turn.",
-            ja: "対戦相手の次のターン中に、対戦相手のポケモンエクスがDeoxys Exに行うダメージを含む攻撃のすべての影響を防ぎます。",
-            fr: "Empêchez tous les effets des attaques, y compris les dégâts, causés à Deoxys ex par le Pokemon-Ex de votre adversaire lors du prochain tour de votre adversaire.",
-            de: "Verhindern Sie alle Auswirkungen von Angriffen, einschließlich Schäden, die Desoxys Ex durch den Pokemon-EX Ihres Gegners in der nächsten Runde Ihres Gegners angerichtet wurden.",
-            es: "Evite todos los efectos de los ataques, incluido el daño, hecho a Deoxys EX por el Pokémon-EX de tu oponente durante el próximo turno de tu oponente.",
-            it: "Prevenire tutti gli effetti degli attacchi, incluso il danno, fatto a Deoxys Ex dal Pokemon-Ex del tuo avversario durante il prossimo turno del tuo avversario.",
-            pt: "Evite todos os efeitos dos ataques, incluindo danos, causados a Deoxys EX pelo Pokemon-Ex do seu oponente durante o próximo turno do seu oponente.",
-          },
+                        ja: "対戦相手の次のターン中に、対戦相手のポケモンエクスがDeoxys Exに行うダメージを含む攻撃のすべての影響を防ぎます。",
+                                                                      },
           damage: 50,
         },
       ],
--- data-asia/PCG/PCG2/060.ts
+++ data-asia/PCG/PCG2/060.ts
@@ -4,14 +4,8 @@ import Set from "../PCG2"
 const card: Card = {
       set: Set,
       name: {
-         en: "Swablu",
-         ja: "Swablu",
-         fr: "Swablu",
-         de: "Swablu",
-         es: "Swablu",
-         it: "Swablu",
-         pt: "Swablu",
-      },
+                  ja: "Swablu",
+                                                   },
 
       rarity: "Uncommon",
       category: "Pokemon",
@@ -23,37 +17,19 @@ const card: Card = {
       abilities: [
         {
           name: {
-            en: "Fluff",
-            ja: "綿毛",
-            fr: "Duvet",
-            de: "Flaum",
-            es: "Pelusa",
-            it: "Lanugine",
-            pt: "Foff",
-          },
+                        ja: "綿毛",
+                                                                      },
           effect: {
-            en: "Whenever Swablu would be damaged or affected by an opponent's attack and already has at least 1 damage counter on it, flip a coin. If heads, prevent all effects of that attack, including damage, done to Swablu.",
-            ja: "Swabluが対戦相手の攻撃によって損傷または影響を受け、すでに少なくとも1つのダメージカウンターがあるときはいつでも、コインをひっくり返します。頭の場合は、Swabluに行われた損害を含む、その攻撃のすべての影響を防ぎます。",
-            fr: "Chaque fois que Swablu serait endommagé ou affecté par l'attaque d'un adversaire et a déjà au moins 1 compteur de dégâts dessus, retournez une pièce. Si les têtes, empêchent tous les effets de cette attaque, y compris les dégâts, causés à Swablu.",
-            de: "Immer wenn Swablu durch den Angriff eines Gegners beschädigt oder betroffen wäre und bereits mindestens einen Schadenschalter hat, drehen Sie eine Münze. Wenn Sie Köpfe sind, verhindern Sie alle Auswirkungen dieses Angriffs, einschließlich Schäden an Swablu.",
-            es: "Cada vez que Swablu sería dañado o afectado por el ataque de un oponente y ya tiene al menos 1 contador de daño, voltea una moneda. Si las cabezas, evitan todos los efectos de ese ataque, incluido el daño, se hacen a Swablu.",
-            it: "Ogni volta che Swablu sarebbe danneggiato o influenzato dall'attacco di un avversario e ha già almeno 1 contatore di danni su di esso, capovolgere una moneta. Se le teste, impediscono tutti gli effetti di quell'attacco, incluso il danno, fatto a Swablu.",
-            pt: "Sempre que Swablu seria danificado ou afetado pelo ataque de um oponente e já possui pelo menos 1 contador de danos, gire uma moeda. Se as cabeças, impedem todos os efeitos desse ataque, incluindo danos, causados ao Swablu.",
-          },
+                        ja: "Swabluが対戦相手の攻撃によって損傷または影響を受け、すでに少なくとも1つのダメージカウンターがあるときはいつでも、コインをひっくり返します。頭の場合は、Swabluに行われた損害を含む、その攻撃のすべての影響を防ぎます。",
+                                                                      },
       }],
 
       attacks: [
         {
           cost: ["Colorless"],
           name: {
-            en: "Peck",
-            ja: "ペック",
-            fr: "Picorer",
-            de: "Picken",
-            es: "Picotear",
-            it: "Peck",
-            pt: "Peck",
-          },
+                        ja: "ペック",
+                                                                      },
           damage: 10,
         },
       ],
