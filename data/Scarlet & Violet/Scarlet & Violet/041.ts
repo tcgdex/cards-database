1ec6343ef56532d52b0cece400fd1d6bc32a1604
aurora <21148213+aurorasmiles@users.noreply.github.com>
2025-09-14 12:09:00 +0200

    feat: variants_detailed
    Update to use new variants_detailed

--- data/Scarlet & Violet/Scarlet & Violet/041.ts
+++ data/Scarlet & Violet/Scarlet & Violet/041.ts
@@ -74,9 +74,14 @@ const card: Card = {
 	retreat: 2,
 	regulationMark: "G",
 
-	variants: {
-		normal: false
-	},
+	variants: [
+		{
+			type: 'reverse'
+		},
+		{
+			type: 'holo'
+		},
+	]
 
 	illustrator: "AKIRA EGAWA",
 
