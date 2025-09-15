6239085f575310d584f496a28ef2deff19d1fc90
thomas-bassett <thomasbassett94@gmail.com>
2025-09-15 20:39:15 +0100

    Remove a mistake

--- data/EX/Deoxys/1.ts
+++ data/EX/Deoxys/1.ts
@@ -99,24 +99,4 @@ const card: Card = {
 	}
 }
 
-
-	variants: [
-		{
-			type: "holo",
-			subtype: "unlimited",
-		},
-		{
-			type: "holo",
-			subtype: "shadowless",
-			stamp : ["1st edition"]
-		},
-		{
-			type: "holo",
-			subtype: "shadowless",
-		},
-		{
-			type: "holo",
-			subtype: "1999-2000-copyright",
-		}
-	],
 export default card
