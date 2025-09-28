724760bad11328552554ce6b9b47a17f03e98828
Avior <git@avior.me>
2025-09-29 00:52:12 +0200

    feat: server works, need testing & validation
    Also currently search is BAD

--- server/src/V2/Components/Serie.ts
+++ server/src/V2/Components/Serie.ts
@@ -1,50 +1,27 @@
 import type { Serie as SDKSerie, SerieResume, SupportedLanguages } from '@tcgdex/sdk'
 import { executeQuery, type Query } from '../../libs/QueryEngine/filter'
 
-import de from '../../../generated/de/series.json'
-import en from '../../../generated/en/series.json'
-import es from '../../../generated/es/series.json'
-import esmx from '../../../generated/es-mx/series.json'
-import fr from '../../../generated/fr/series.json'
-import id from '../../../generated/id/series.json'
-import it from '../../../generated/it/series.json'
-import ja from '../../../generated/ja/series.json'
-import ko from '../../../generated/ko/series.json'
-import nl from '../../../generated/nl/series.json'
-import pl from '../../../generated/pl/series.json'
-import ptbr from '../../../generated/pt-br/series.json'
-import ptpt from '../../../generated/pt-pt/series.json'
-import pt from '../../../generated/pt/series.json'
-import ru from '../../../generated/ru/series.json'
-import th from '../../../generated/th/series.json'
-import zhcn from '../../../generated/zh-cn/series.json'
-import zhtw from '../../../generated/zh-tw/series.json'
+import series from '../../../generated/series.json'
+import { CompiledSerie } from '../../../../scripts/compiler/interfaces'
+import { objectKeys } from '@dzeio/object-util'
 
-const series = {
-	en: en,
-	fr: fr,
-	es: es,
-	'es-mx': esmx,
-	it: it,
-	pt: pt,
-	'pt-br': ptbr,
-	'pt-pt': ptpt,
-	de: de,
-	nl: nl,
-	pl: pl,
-	ru: ru,
-	ja: ja,
-	ko: ko,
-	'zh-tw': zhtw,
-	id: id,
-	th: th,
-	'zh-cn': zhcn,
-} as const
+
+// Compile data subsets
+const list: Record<string, CompiledSerie> = {}
+const langLists: Record<string, Array<CompiledSerie>> = {}
+
+series.forEach((it) => {
+	list[it.id.toLowerCase()] = it
+	objectKeys(it.name).forEach((lang) => {
+		langLists[lang] ??= []
+		langLists[lang].push(it)
+	})
+})
 
 type MappedSerie = any // (typeof en)[number]
 
 export async function getAllSeries(lang: SupportedLanguages): Promise<Array<SDKSerie>> {
-	return Promise.all((series[lang] as Array<MappedSerie>).map(transformSerie))
+	return Promise.all((langLists[lang] as Array<MappedSerie>).map(transformSerie))
 }
 
 async function transformSerie(serie: MappedSerie): Promise<SDKSerie> {
--- server/src/V2/Components/Set.ts
+++ server/src/V2/Components/Set.ts
@@ -1,54 +1,47 @@
 import type { Set as SDKSet, SetResume, SupportedLanguages } from '@tcgdex/sdk'
 import { executeQuery, type Query } from '../../libs/QueryEngine/filter'
-import { objectOmit } from '@dzeio/object-util'
+import { objectKeys, objectOmit } from '@dzeio/object-util'
 
-import de from '../../../generated/de/sets.json'
-import en from '../../../generated/en/sets.json'
-import es from '../../../generated/es/sets.json'
-import esmx from '../../../generated/es-mx/sets.json'
-import fr from '../../../generated/fr/sets.json'
-import id from '../../../generated/id/sets.json'
-import it from '../../../generated/it/sets.json'
-import ja from '../../../generated/ja/sets.json'
-import ko from '../../../generated/ko/sets.json'
-import nl from '../../../generated/nl/sets.json'
-import pl from '../../../generated/pl/sets.json'
-import ptbr from '../../../generated/pt-br/sets.json'
-import ptpt from '../../../generated/pt-pt/sets.json'
-import pt from '../../../generated/pt/sets.json'
-import ru from '../../../generated/ru/sets.json'
-import th from '../../../generated/th/sets.json'
-import zhcn from '../../../generated/zh-cn/sets.json'
-import zhtw from '../../../generated/zh-tw/sets.json'
+import dataTMP from '../../../generated/sets.json'
+import { CompiledSet } from '../../../../scripts/compiler/interfaces'
+import { Version } from '../../interfaces'
 
-export const sets = {
-	en: en,
-	fr: fr,
-	es: es,
-	'es-mx': esmx,
-	it: it,
-	pt: pt,
-	'pt-br': ptbr,
-	'pt-pt': ptpt,
-	de: de,
-	nl: nl,
-	pl: pl,
-	ru: ru,
-	ja: ja,
-	ko: ko,
-	'zh-tw': zhtw,
-	id: id,
-	th: th,
-	'zh-cn': zhcn,
-} as const
+const data = dataTMP as Array<CompiledSet>
+
+// Compile data subsets
+const list: Record<string, CompiledSet> = {}
+export const langLists: Record<string, Array<CompiledSet>> = {}
+
+data.forEach((it) => {
+	list[it.id.toLowerCase()] = it
+	objectKeys(it.name).forEach((lang) => {
+		langLists[lang] ??= []
+		langLists[lang].push(it)
+	})
+})
 
 type MappedSet = any // (typeof en)[number]
 
-export async function getAllSets(lang: SupportedLanguages): Promise<Array<SDKSet>> {
-	return Promise.all((sets[lang] as Array<MappedSet>).map(transformSet))
+export async function getAllSets(lang: SupportedLanguages, version: Version = 'full'): Promise<Array<SDKSet>> {
+	return Promise.all((langLists[lang] as Array<MappedSet>).map((it) => transformSet(it, lang, version as 'full')))
 }
 
-async function transformSet(set: MappedSet): Promise<SDKSet> {
+
+async function transformSet(set: MappedSet, lang: SupportedLanguages, version?: 'full'): Promise<SDKSet>
+async function transformSet(set: MappedSet, lang: SupportedLanguages, version: 'brief'): Promise<SetResume>
+async function transformSet(set: MappedSet, lang: SupportedLanguages, version: Version = 'full'): Promise<SDKSet | SetResume> {
+	if (version === 'brief') {
+		return {
+			id: set.id,
+			name: set.name[lang]!,
+			logo: set.logo[lang]!,
+			symbol: set.symbol,
+			cardCount: {
+				total: set.cardCount.total,
+				official: set.cardCount.official
+			}
+		}
+	}
 	return {
 		...objectOmit(set, 'thirdParty'),
 		// pricing: {
@@ -58,8 +51,8 @@ async function transformSet(set: MappedSet): Promise<SDKSet> {
 	}
 }
 
-export async function findSets(lang: SupportedLanguages, query: Query<SDKSet>) {
-	return executeQuery(await getAllSets(lang), query).data
+export async function findSets(lang: SupportedLanguages, query: Query<SDKSet>, version: Version = 'full') {
+	return executeQuery(await getAllSets(lang, version), query).data
 }
 
 export async function findOneSet(lang: SupportedLanguages, query: Query<SDKSet>) {
@@ -68,6 +61,13 @@ export async function findOneSet(lang: SupportedLanguages, query: Query<SDKSet>)
 		return undefined
 	}
 	return res[0]
+}
+
+export async function loadSet(id: string, lang: SupportedLanguages, version: 'full'): Promise<SDKSet>
+export async function loadSet(id: string, lang: SupportedLanguages, version: 'brief'): Promise<SetResume>
+export async function loadSet(id: string, lang: SupportedLanguages, version: Version = 'full'): Promise<SDKSet | SetResume> {
+	const tmp = langLists[lang].find((it) => it.id === id)
+	return transformSet(tmp, lang, version as 'full')
 }
 
 export function setToBrief(set: SDKSet): SetResume {
