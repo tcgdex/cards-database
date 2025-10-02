0e52c881e0f0f67a539929c8c2aa2801123f857d
Yinan Wang <yinan@lucidcorp.com>
2025-10-02 02:27:59 -0400

    Added 20/86

---
+++ data-asia/SV/SV11W/011.ts
@@ -0,0 +1,42 @@
+import { Card } from "../../../interfaces";
+import Set from "../SV11W";
+
+const card: Card = {
+	set: Set,
+
+	name: {
+		ja: "ポカブ",
+	},
+
+	illustrator: "Teeziro",
+	category: "Pokemon",
+	hp: 70,
+	types: ["Fire"],
+
+	description: {
+		ja: "敵の 攻撃を 身軽に 避けて 鼻から 火の玉を 撃ち出す。 炎で 木の実を 焼いて 食べる。",
+	},
+
+	stage: "Basic",
+
+	attacks: [
+		{ name: { ja: "たいあたり" }, damage: 10, cost: ["Fire"] },
+		{ name: { ja: "ころがる" }, damage: 30, cost: ["Fire", "Fire"] },
+	],
+
+	weaknesses: [{ type: "Water", value: "×2" }],
+
+	variants: {
+		normal: true,
+		reverse: true,
+		holo: false,
+		firstEdition: false,
+	},
+
+	retreat: 1,
+	regulationMark: "I",
+	rarity: "Common",
+	dexId: [498],
+};
+
+export default card;
