a83b664e5119a41df35f4cc118b99d484c038916
thomas-bassett <91132581+thomas-bassett@users.noreply.github.com>
2025-10-15 21:18:34 +0100

    Add SVP Black Star Promos cards 208-212 (#925)

---
+++ data/Scarlet & Violet/SVP Black Star Promos/211.ts
@@ -0,0 +1,63 @@
+import { Card } from "../../../interfaces"
+import Set from "../SVP Black Star Promos"
+
+const card: Card = {
+	dexId: [576],
+	set: Set,
+
+	name: {
+		en: "Gothitelle",
+	},
+
+	rarity: "None",
+	category: "Pokemon",
+	hp: 150,
+	types: ["Colorless"],
+	stage: "Stage2",
+	illustrator: "IKEDA Saki",
+
+	attacks: [
+		{
+			cost: ["Psychic","Colorless"],
+
+			name: {
+				en: "Synchro Shot",
+			},
+
+			effect: {
+				en: "If you have the same number of cards in your hand as your opponent, this attack does 90 more damage.",
+			},
+			damage: "90+"
+		}
+	],
+
+	weaknesses: [
+		{
+			type: "Darkness",
+			value: "×2"
+		}
+	],
+
+	resistances: [
+		{
+			type: "Fighting",
+			value: "-30"
+		}
+	],
+
+	retreat: 2,
+	regulationMark: "I",
+
+	variants: [
+		{
+			type: "holo"
+		},
+	],
+
+	thirdParty: {
+		cardmarket: 836659,
+		tcgplayer: 647306,
+	}
+}
+
+export default card
