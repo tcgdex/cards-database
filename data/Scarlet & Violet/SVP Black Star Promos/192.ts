248424db92b072c6215eaa6bb05a0bf9982f8fa9
thomas-bassett <thomasbassett94@gmail.com>
2025-10-16 22:08:44 +0100

    Add new SVP Black Star Promos cards and update variants
    Added multiple new card files for the Scarlet & Violet SVP Black Star Promos set. Updated interfaces.d.ts to support new variant types, subtypes, stamps, and foils. Extended translation files (de, es, fr, it, pt) to include new variant and stamp terms for recent promos and events.

---
+++ data/Scarlet & Violet/SVP Black Star Promos/192.ts
@@ -0,0 +1,47 @@
+import { Card } from "../../../interfaces"
+import Set from "../SVP Black Star Promos"
+
+const card: Card = {
+	set: Set,
+
+	name: {
+		en: "Fuecoco",
+	},
+
+	rarity: "None",
+	category: "Pokemon",
+	hp: 80,
+	types: ["Fire"],
+	stage: "Basic",
+	dexId: [909],
+	weaknesses: [{
+		type: "Water",
+		value: "×2"
+	}],
+
+	illustrator: "Ito Kyoko",
+
+	attacks: [
+		{
+			cost: ["Fire"],
+
+			name: {
+				en: "Tackle",
+			},
+
+			damage: "20"
+		},
+	],
+
+	retreat: 2,
+	regulationMark: "G",
+
+	variants:[
+		{
+			type: "holo",
+			stamp: ["horizons"]
+		}
+	]
+}
+
+export default card
