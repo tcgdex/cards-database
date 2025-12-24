35900f6b29f770876568b54587778e8f4902740b
RikkaCoder <riccardoscommessa@gmail.com>
2025-12-24 12:54:17 +0100

    feat(data): Add Italian evolution for Alakazam, Blastoise, Charizard

--- data/Base/Base Set/1.ts
+++ data/Base/Base Set/1.ts
@@ -26,6 +26,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Kadabra",
+		it: "Kadabra"
 	},
 
 	stage: "Stage2",
--- data/Base/Base Set/4.ts
+++ data/Base/Base Set/4.ts
@@ -26,6 +26,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Charmeleon",
+		it: "Charmeleon"
 	},
 
 	stage: "Stage2",
