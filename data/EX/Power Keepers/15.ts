bac0cfce2dfd09ce18c625888edbc2ecd64e45ad
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 01:24:13 +0100

    Power Keepers

--- data/EX/Power Keepers/15.ts
+++ data/EX/Power Keepers/15.ts
@@ -25,6 +25,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Chinchou",
+		fr: "Loupio"
 	},
 
 	stage: "Stage1",
--- data/EX/Power Keepers/97.ts
+++ data/EX/Power Keepers/97.ts
@@ -25,6 +25,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Nuzleaf",
+		fr: "Pifeuil"
 	},
 
 	suffix: "EX",
