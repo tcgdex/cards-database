7a5cbc1375c6912891c9138e4d913936302a98ab
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 01:24:47 +0100

    McDonald's Collection 2017

--- data/McDonald's Collection/Macdonald's Collection 2017/10.ts
+++ data/McDonald's Collection/Macdonald's Collection 2017/10.ts
@@ -2,29 +2,44 @@ import { Card } from '../../../interfaces'
 import Set from '../Macdonald\'s Collection 2017'
 
 const card: Card = {
-	dexId: [742],
+	dexId: [
+		742,
+	],
 	set: Set,
-
 	variants: {
 		normal: false,
 		reverse: false,
 		holo: false,
-		firstEdition: false
+		firstEdition: false,
 	},
-
 	name: {
-		en: "Cutiefly"
+		en: "Cutiefly",
+		fr: "Bombydou",
 	},
-
 	rarity: "None",
 	category: "Pokemon",
 	hp: 30,
-	types: ["Fairy"],
+	types: [
+		"Fairy",
+	],
 	stage: "Basic",
-
 	thirdParty: {
-		tcgplayer: 152690
-	}
+		tcgplayer: 152690,
+	},
+	attacks: [
+		{
+			cost: [
+				"Fairy",
+			],
+			name: {
+				fr: "Papillonnement",
+			},
+			damage: "10",
+			effect: {
+				fr: "Si des dégâts sont infligés à ce Pokémon par des attaques pendant le prochain tour de votre adversaire, lancez une pièce. Si c'est face, évitez ces dégâts.",
+			},
+		},
+	],
 }
 
 export default card
--- data/McDonald's Collection/Macdonald's Collection 2017/12.ts
+++ data/McDonald's Collection/Macdonald's Collection 2017/12.ts
@@ -2,29 +2,51 @@ import { Card } from '../../../interfaces'
 import Set from '../Macdonald\'s Collection 2017'
 
 const card: Card = {
-	dexId: [734],
+	dexId: [
+		734,
+	],
 	set: Set,
-
 	variants: {
 		normal: false,
 		reverse: false,
 		holo: false,
-		firstEdition: false
+		firstEdition: false,
 	},
-
 	name: {
-		en: "Yungoos"
+		en: "Yungoos",
+		fr: "Manglouton",
 	},
-
 	rarity: "None",
 	category: "Pokemon",
 	hp: 70,
-	types: ["Colorless"],
+	types: [
+		"Colorless",
+	],
 	stage: "Basic",
-
 	thirdParty: {
-		tcgplayer: 152693
-	}
+		tcgplayer: 152693,
+	},
+	attacks: [
+		{
+			cost: [
+				"Colorless",
+			],
+			name: {
+				fr: "Charge",
+			},
+			damage: "10",
+		},
+		{
+			cost: [
+				"Colorless",
+				"Colorless",
+			],
+			name: {
+				fr: "Morsure",
+			},
+			damage: "20",
+		},
+	],
 }
 
 export default card
