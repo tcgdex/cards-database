43e939c1a684b3a454c3f1164e73ccb8d157481c
jmandev88 <talk@josephman.co.uk>
2026-01-07 12:24:25 +0000

    Fix: Update PHL variants (#1039)

--- data/Mega Evolution/Phantasmal Flames/085.ts
+++ data/Mega Evolution/Phantasmal Flames/085.ts
@@ -30,6 +30,15 @@ const card: Card = {
 	trainerType: "Stadium",
 	regulationMark: "I",
 
+	variants: [
+		{
+			type: "normal"
+		},
+		{
+			type: "reverse"
+		}
+	],
+
 	thirdParty: {
 		cardmarket: 857660
 	}
--- data/Mega Evolution/Phantasmal Flames/094.ts
+++ data/Mega Evolution/Phantasmal Flames/094.ts
@@ -30,6 +30,15 @@ const card: Card = {
 	trainerType: "Item",
 	regulationMark: "I",
 
+	variants: [
+		{
+			type: "normal"
+		},
+		{
+			type: "reverse"
+		}
+	],
+
 	thirdParty: {
 		cardmarket: 857669
 	}
