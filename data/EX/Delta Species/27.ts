65befc4c3ecf27f3f1b8601c39daf390d703043a
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 01:24:11 +0100

    Delta Species

--- data/EX/Delta Species/27.ts
+++ data/EX/Delta Species/27.ts
@@ -26,6 +26,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Sandshrew",
+		fr: "Sabelette"
 	},
 
 	stage: "Stage1",
--- data/EX/Delta Species/5.ts
+++ data/EX/Delta Species/5.ts
@@ -26,6 +26,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Eevee",
+		fr: "Évoli"
 	},
 
 	stage: "Stage1",
