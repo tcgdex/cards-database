5f79fdaca3f4c767cdab1e88557243bff36a18bb
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 01:24:45 +0100

    McDonald's Collection 2014

--- data/McDonald's Collection/Macdonald's Collection 2014/12.ts
+++ data/McDonald's Collection/Macdonald's Collection 2014/12.ts
@@ -2,29 +2,56 @@ import { Card } from '../../../interfaces'
 import Set from '../Macdonald\'s Collection 2014'
 
 const card: Card = {
-	dexId: [676],
+	dexId: [
+		676,
+	],
 	set: Set,
-
 	variants: {
 		normal: false,
 		reverse: false,
 		holo: true,
-		firstEdition: false
+		firstEdition: false,
 	},
-
 	name: {
-		en: "Furfrou"
+		en: "Furfrou",
+		fr: "Couafarel",
 	},
-
 	rarity: "None",
 	category: "Pokemon",
 	hp: 90,
-	types: ["Colorless"],
+	types: [
+		"Colorless",
+	],
 	stage: "Basic",
-
 	thirdParty: {
-		tcgplayer: 110417
-	}
+		tcgplayer: 110417,
+	},
+	attacks: [
+		{
+			cost: [
+				"Colorless",
+				"Colorless",
+			],
+			name: {
+				fr: "Mâchoire Serrée",
+			},
+			damage: "20",
+			effect: {
+				fr: "Lancez une pièce. Si c'est face, le Pokémon Actif de votre adversaire est maintenant Paralysé.",
+			},
+		},
+		{
+			cost: [
+				"Colorless",
+				"Colorless",
+				"Colorless",
+			],
+			name: {
+				fr: "Croc Aiguisé",
+			},
+			damage: "50",
+		},
+	],
 }
 
 export default card
