7cb93aa623a703cc7352e2cf03c44957b4aed4b1
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 10:17:01 +0100

    fix: Correct syntax error in DP trainer Kit (Lucario) card 10 effect

--- data/Trainer kits/DP trainer Kit (Lucario)/10.ts
+++ data/Trainer kits/DP trainer Kit (Lucario)/10.ts
@@ -13,9 +13,9 @@ const card: Card = {
 	set: Set,
 
 	effect: {
-		en: "Reveal cards from your deck until you reveal a Pokémon. Show that Pokémon to your opponent and put it into your hand. ",
-		fr: "Retournez des cartes de votre deck jusqu'à ce que vous retourniez un Pokémon. Montrez-le à votre adversaire et placez-le dans votre main. Re-mélangez les autres cartes à votre deck. (Si vous ne retournez pas de Pokémon, re-mélangez toutes les cartes à votre deck.)", +
-			"Shuffle the other revealed cards back into your deck. (If you don't reveal a Pokémon, shuffle all the revealed cards back into your deck.)"
+		en: "Reveal cards from your deck until you reveal a Pokémon. Show that Pokémon to your opponent and put it into your hand. " +
+			"Shuffle the other revealed cards back into your deck. (If you don't reveal a Pokémon, shuffle all the revealed cards back into your deck.)",
+		fr: "Retournez des cartes de votre deck jusqu'à ce que vous retourniez un Pokémon. Montrez-le à votre adversaire et placez-le dans votre main. Re-mélangez les autres cartes à votre deck. (Si vous ne retournez pas de Pokémon, re-mélangez toutes les cartes à votre deck.)"
 	}
 }
 
