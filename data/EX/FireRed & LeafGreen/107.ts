1bc31fafe833eb4134d1675cbac865e7e752da73
Laurent AMPLIS <lamplis@yahoo.fr>
2026-01-07 01:24:09 +0100

    FireRed & LeafGreen

--- data/EX/FireRed & LeafGreen/107.ts
+++ data/EX/FireRed & LeafGreen/107.ts
@@ -25,6 +25,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Voltorb",
+		fr: "Voltorbe"
 	},
 
 	suffix: "EX",
--- data/EX/FireRed & LeafGreen/17.ts
+++ data/EX/FireRed & LeafGreen/17.ts
@@ -25,6 +25,7 @@ const card: Card = {
 
 	evolveFrom: {
 		en: "Weepinbell",
+		fr: "Boustiflor"
 	},
 
 	stage: "Stage2",
