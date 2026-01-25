3f12d5df9cec9a38588f08182af2704dd8d1e8c2
Yu Ohashi <yuyake0084@gmail.com>
2026-01-26 02:14:09 +0900

    add M1S set

---
+++ data-asia/MEGA/M1S.ts
@@ -0,0 +1,21 @@
+import { Set } from '../../interfaces'
+import serie from '../MEGA'
+
+const set: Set = {
+	id: 'M1S',
+	name: {
+		ja: 'メガシンフォニア'
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 63
+	},
+
+	releaseDate: {
+		ja: '2025-08-01'
+	}
+}
+
+export default set
