1c3e5836f3e7f0d4aee54b09f6cab57e33c9e05a
elpekenin <elpekenin@elpekenin.dev>
2026-02-07 13:36:49 +0100

    chore: update variants for POP/6

--- data/POP/POP Series 6/3.ts
+++ data/POP/POP Series 6/3.ts
@@ -70,12 +70,9 @@ const card: Card = {
 
 	retreat: 1,
 
-	variants: {
-		normal: false,
-		reverse: false,
-		holo: true,
-		firstEdition: false
-	},
+	variants: [
+		{ type: 'holo', size: 'standard' }
+	],
 
 	thirdParty: {
 		cardmarket: 277888,
