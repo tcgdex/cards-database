7ae3acb725e8203de2d49e630bcfc74247c1f712
Arthur Migne <arthur.migne44@gmail.com>
2026-03-22 11:54:42 +0100

    feat: Add rare cards of Paldean Wonders

---
+++ data/Pokémon TCG Pocket/Paldean Wonders/115.ts
@@ -0,0 +1,23 @@
+import { Card } from "../../../interfaces";
+import Set from "../Paldean Wonders";
+
+const card: Card = {
+    set: Set,
+
+    name: {
+        en: "Arven",
+        fr: "Pepper"
+    },
+
+    illustrator: "GIDORA",
+    rarity: "Three Star",
+    category: "Trainer",
+
+    effect: {
+        en: "Flip a coin. If heads, put a random Item card from your deck into your hand. If tails, put a random Pokémon Tool card from your deck into your hand.",
+    },
+    
+    trainerType: "Supporter",
+}
+
+export default card
