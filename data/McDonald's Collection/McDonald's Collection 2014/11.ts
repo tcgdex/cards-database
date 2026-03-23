6597909fe259bbd94f027121816a140e2830de13
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:04 +0100

    fix(data): update McDonald's Collection / McDonald's Collection 2014

---
+++ data/McDonald's Collection/McDonald's Collection 2014/11.ts
@@ -0,0 +1,45 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2014'
+
+const card: Card = {
+	dexId: [
+		661,
+	],
+	set: Set,
+	variants: {
+		normal: false,
+		reverse: false,
+		holo: true,
+		firstEdition: false,
+	},
+	name: {
+		en: "Fletchling",
+		fr: "Passerouge",
+	},
+	rarity: "None",
+	category: "Pokemon",
+	hp: 50,
+	types: [
+		"Colorless",
+	],
+	stage: "Basic",
+	thirdParty: {
+		tcgplayer: 110416,
+	},
+	attacks: [
+		{
+			cost: [
+				"Colorless",
+			],
+			name: {
+				fr: "Coupe-Vent",
+			},
+			damage: "20",
+			effect: {
+				fr: "Lancez une pièce. Si c'est pile, cette attaque ne fait rien.",
+			},
+		},
+	],
+}
+
+export default card
