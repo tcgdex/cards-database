3c9eb467486701ef9334d9e19371bf7cd4c0b891
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:40 +0100

    fix(data): update Mega Evolution / Perfect Order

---
+++ data/Mega Evolution/Perfect Order.ts
@@ -0,0 +1,21 @@
+import { Set } from '../../interfaces'
+import serie from '../Mega Evolution'
+
+const set: Set = {
+	id: "me03",
+
+	name: {
+		en: "Perfect Order",
+		fr: "Équilibre Parfait"
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 0
+	},
+
+	releaseDate: "2026-04-11",
+}
+
+export default set
