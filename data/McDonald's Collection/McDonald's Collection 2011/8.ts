924d4774cd09c93a0daf58ffc3b1f9fd24c67401
lamplis <lamplis@users.noreply.github.com>
2026-03-23 10:53:53 +0100

    fix(data): update McDonald's Collection / McDonald's Collection 2011 (#1226)

---
+++ data/McDonald's Collection/McDonald's Collection 2011/8.ts
@@ -0,0 +1,52 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2011'
+
+const card: Card = {
+	set: Set,
+	illustrator: "Ken Sugimori",
+	category: "Pokemon",
+
+	dexId: [551],
+
+	description: {
+		en: "It moves along below the sand’s surface, except for its nose and eyes. A dark membrane shields its eyes from the sun."
+	},
+
+	stage: "Basic",
+
+	attacks: [{
+		name: {
+			en: "Sand Tomb",
+			fr: "Tourbi-Sable"
+		},
+
+		damage: 30,
+
+		effect: {
+			en: "The Defending Pokémon can’t retreat during your opponent’s next turn.",
+			fr: "Le Pokémon Défenseur ne peut pas battre en retraite durant le prochain tour de votre adversaire."
+		}
+	}],
+
+	variants: {
+		normal: false,
+		reverse: false,
+		holo: true,
+		firstEdition: false
+	},
+
+	name: {
+		en: "Sandile",
+		fr: "Mascaïman"
+	},
+
+	rarity: "None",
+	hp: 70,
+	types: ["Fighting"],
+
+	thirdParty: {
+		tcgplayer: 88913
+	}
+}
+
+export default card
