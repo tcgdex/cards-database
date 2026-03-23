d13ed069bd5ebc74918661337b2f0d9f2e27d08c
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:54 +0100

    fix(data): update POP / POP Series 2

--- data/POP/POP Series 2/8.ts
+++ data/POP/POP Series 2/8.ts
@@ -15,7 +15,7 @@ const card: Card = {
 
 	effect: {
 		en: "Choose 1 of your Pokémon in play (excluding Pokémon-ex). Return that Pokémon and all cards attached to it to your hand.",
-		fr: "Vous ne pouvez jouer qu'une seule carte Supporter par tour. Lorsque vous jouez cette carte, placez-la à côté de votre Pokémon Actif. À la fin du tour, défaussez-la.\n\nChoisissez 1 des Pokémon que vous avez en jeu (sauf les Pokémon-ex). Reprenez dans votre main ce Pokémon ainsi que toutes les cartes qui lui sont attachées."
+		fr: "Choisissez 1 des Pokémon que vous avez en jeu (sauf les Pokémon-ex). Reprenez dans votre main ce Pokémon ainsi que toutes les cartes qui lui sont attachées.",
 	},
 
 	variants: {
