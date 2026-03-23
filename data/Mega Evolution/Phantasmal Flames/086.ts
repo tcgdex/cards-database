9b0b564a25c68ed9518fb4d409c9c0682b95768b
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-24 00:52:32 +0100

    fix(data): update Mega Evolution / Phantasmal Flames

--- data/Mega Evolution/Phantasmal Flames/086.ts
+++ data/Mega Evolution/Phantasmal Flames/086.ts
@@ -19,12 +19,12 @@ const card: Card = {
 
 	effect: {
 		en: "You can use this card only if you discard a Basic {R} Energy card from your hand.\n\nDiscard a Pokémon Tool or Special Energy card from 1 of your opponent's Pokémon, or discard a Stadium in play.",
-		fr: "Vous ne pouvez utiliser cette carte que si vous défaussez une carte Énergie {R} de base de votre main.\n\nDéfaussez une carte Outil Pokémon ou Énergie spéciale de l'un des Pokémon de votre adversaire, ou défaussez un Stade en jeu.",
+		fr: "Vous ne pouvez utiliser cette carte que si vous défaussez une carte Énergie Feu de base de votre main.",
 		es: "Puedes usar esta carta solo si descartas una carta de Energía {R} Básica de tu mano.\n\nDescarta 1 carta de Herramienta Pokémon o de Energía Especial de uno de los Pokémon de tu rival, o descarta 1 Estadio en juego.",
 		'es-mx': "Solo puedes usar esta carta si descartas 1 carta de Energía {R} Básica de tu mano.\n\nDescarta 1 carta de Herramienta Pokémon o de Energía Especial de 1 de los Pokémon de tu rival, o descarta 1 Estadio en juego.",
 		de: "Du kannst diese Karte nur einsetzen, wenn du 1 Basis-{R}-Energiekarte aus deiner Hand auf deinen Ablagestapel legst.\n\nLege 1 Pokémon-Ausrüstung oder Spezial-Energiekarte von 1 Pokémon deines Gegners auf seinen Ablagestapel oder lege 1 Stadion im Spiel auf den Ablagestapel.",
 		it: "Puoi usare questa carta solo se scarti una carta Energia base {R} che hai in mano.\n\nScarta una carta Oggetto Pokémon o una carta Energia speciale da uno dei Pokémon del tuo avversario o scarta una carta Stadio in gioco.",
-		pt: "Você só pode usar esta carta se descartar uma carta de Energia {R} Básica da sua mão.\n\nDescarte uma carta de Ferramenta Pokémon ou Energia Especial de 1 dos Pokémon do seu oponente, ou descarte um Estádio em jogo."
+		pt: "Você só pode usar esta carta se descartar uma carta de Energia {R} Básica da sua mão.\n\nDescarte uma carta de Ferramenta Pokémon ou Energia Especial de 1 dos Pokémon do seu oponente, ou descarte um Estádio em jogo.",
 	},
 
 	trainerType: "Item",
