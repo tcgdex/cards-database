abb16fc2a0974dbd60619335de64f6afd464d3d2
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:13:58 +0100

    fix(data): update Sun & Moon / Team Up

--- data/Sun & Moon/Team Up/194.ts
+++ data/Sun & Moon/Team Up/194.ts
@@ -17,18 +17,18 @@ const card: Card = {
 	set: Set,
 
 	effect: {
-		fr: "Choisissez l’une de ces options :\n\n• Piochez une carte.\n• Placez une carte Juge de votre pile de défausse dans votre main.",
+		fr: "Placez une carte Juge de votre pile de défausse dans votre main.",
 		en: "Choose 1:\n\n• Draw a card.\n• Put a Judge card from your discard pile into your hand.",
 		es: "Elige 1 opción:\n\n• Roba 1 carta.\n• Pon 1 carta de Juez de tu pila de descartes en tu mano.",
 		it: "Scegli:\n\n• Pesca una carta.\n• Prendi una carta Arbitro dalla tua pila degli scarti e aggiungila alle carte che hai in mano.",
 		pt: "Escolha 1:\n\n• Compre 1 carta.\n• Coloque 1 carta Juiz da sua pilha de descarte na sua mão.",
-		de: "Wähle 1 aus:\n\n• Ziehe 1 Karte.\n• Nimm 1 Richter-Karte aus deinem Ablagestapel auf deine Hand."
+		de: "Wähle 1 aus:\n\n• Ziehe 1 Karte.\n• Nimm 1 Richter-Karte aus deinem Ablagestapel auf deine Hand.",
 	},
 
 	trainerType: "Item",
 
 	thirdParty: {
-		cardmarket: 369119,
+		cardmarket: 369072,
 		tcgplayer: 183944
 	}
 }
