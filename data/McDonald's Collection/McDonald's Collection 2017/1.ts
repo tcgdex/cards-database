a8aff0998f03085aeabe9befb4b0570e86f6146e
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:10 +0100

    fix(data): update McDonald's Collection / McDonald's Collection 2017

---
+++ data/McDonald's Collection/McDonald's Collection 2017/1.ts
@@ -0,0 +1,52 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2017'
+
+const card: Card = {
+	dexId: [
+		722,
+	],
+	set: Set,
+	variants: {
+		normal: false,
+		reverse: false,
+		holo: false,
+		firstEdition: false,
+	},
+	name: {
+		en: "Rowlet",
+		fr: "Brindibou",
+	},
+	rarity: "None",
+	category: "Pokemon",
+	hp: 60,
+	types: [
+		"Grass",
+	],
+	stage: "Basic",
+	thirdParty: {
+		tcgplayer: 152681,
+	},
+	attacks: [
+		{
+			cost: [
+				"Colorless",
+			],
+			name: {
+				fr: "Charge",
+			},
+			damage: "10",
+		},
+		{
+			cost: [
+				"Grass",
+				"Colorless",
+			],
+			name: {
+				fr: "Feuillage",
+			},
+			damage: "20",
+		},
+	],
+}
+
+export default card
---
+++ data/McDonald's Collection/McDonald's Collection 2017/3.ts
@@ -0,0 +1,52 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2017'
+
+const card: Card = {
+	dexId: [
+		725,
+	],
+	set: Set,
+	variants: {
+		normal: false,
+		reverse: false,
+		holo: false,
+		firstEdition: false,
+	},
+	name: {
+		en: "Litten",
+		fr: "Flamiaou",
+	},
+	rarity: "None",
+	category: "Pokemon",
+	hp: 70,
+	types: [
+		"Fire",
+	],
+	stage: "Basic",
+	thirdParty: {
+		tcgplayer: 152683,
+	},
+	attacks: [
+		{
+			cost: [
+				"Fire",
+			],
+			name: {
+				fr: "Morsure",
+			},
+			damage: "10",
+		},
+		{
+			cost: [
+				"Fire",
+				"Colorless",
+			],
+			name: {
+				fr: "Flamboiement",
+			},
+			damage: "20",
+		},
+	],
+}
+
+export default card
