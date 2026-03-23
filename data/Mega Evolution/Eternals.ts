b1b8edca70ad08a2cf04af7c12320922d83a35d5
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:34 +0100

    fix(data): update Mega Evolution / Eternals

---
+++ data/Mega Evolution/Eternals.ts
@@ -0,0 +1,21 @@
+import { Set } from '../../interfaces'
+import serie from '../Mega Evolution'
+
+const set: Set = {
+	id: "me05",
+
+	name: {
+		en: "Eternals",
+		fr: "Éternels"
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 0
+	},
+
+	releaseDate: "2026-10-16",
+}
+
+export default set
