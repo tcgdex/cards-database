33a0bf4146d8190caad02f44f1b377110ca216b2
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:13:18 +0100

    fix(data): update Scarlet & Violet / Destined Rivals

--- data/Scarlet & Violet/Destined Rivals/227.ts
+++ data/Scarlet & Violet/Destined Rivals/227.ts
@@ -22,12 +22,12 @@ const card: Card = {
 
 	effect: {
 		en: "If you go first, you may use this card during your first turn.\n\nSearch your deck for up to 3 Basic Team Rocket's Pokémon, reveal them, and put them into your hand. Then, shuffle your deck.",
-		fr: "Si vous jouez en premier, vous pouvez utiliser cette carte pendant votre premier tour.\n\nCherchez dans votre deck jusqu'à 3 Pokémon de la Team Rocket de base, montrez-les, puis ajoutez-les à votre main. Mélangez ensuite votre deck.",
+		fr: "Si vous jouez en premier, vous pouvez utiliser cette carte pendant votre premier tour.",
 		de: "Wenn du als Erster am Zug bist, kannst du diese Karte während deines ersten Zuges einsetzen.\n\nDurchsuche dein Deck nach bis zu 3 Basis-Team-Rockets-Pokémon, zeige sie deinem Gegner und nimm sie auf deine Hand. Mische anschließend dein Deck.",
 		it: "Se inizi per primo, puoi usare questa carta durante il tuo primo turno.\n\nCerca nel tuo mazzo fino a tre Pokémon del Team Rocket Base, mostrali e aggiungili alle carte che hai in mano. Poi rimischia le carte del tuo mazzo.",
 		es: "Si sales en primer lugar, puedes usar esta carta durante tu primer turno.\n\nBusca en tu baraja hasta 3 Pokémon del Team Rocket Básicos, enséñalos y ponlos en tu mano. Después, baraja las cartas de tu baraja.",
 		pt: "Se você for o primeiro a jogar, poderá usar esta carta durante o seu primeiro turno.\n\nProcure por até 3 Pokémon da Equipe Rocket Básicos no seu baralho, revele-os e coloque-os na sua mão. Em seguida, embaralhe o seu baralho.",
-		'es-mx': "Si sales en primer lugar, puedes usar esta carta durante tu primer turno.\n\nBusca en tu mazo hasta 3 Pokémon del Equipo Rocket Básicos, muéstralos y ponlos en tu mano. Después, baraja las cartas de tu mazo."
+		'es-mx': "Si sales en primer lugar, puedes usar esta carta durante tu primer turno.\n\nBusca en tu mazo hasta 3 Pokémon del Equipo Rocket Básicos, muéstralos y ponlos en tu mano. Después, baraja las cartas de tu mazo.",
 	},
 
 	trainerType: "Supporter",
