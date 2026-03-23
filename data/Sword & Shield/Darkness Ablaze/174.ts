5d47d515b0086ed976f38ce050bba54abafc9574
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:14:16 +0100

    fix(data): update Sword & Shield / Darkness Ablaze

--- data/Sword & Shield/Darkness Ablaze/174.ts
+++ data/Sword & Shield/Darkness Ablaze/174.ts
@@ -18,11 +18,11 @@ const card: Card = {
 
 	effect: {
 		en: "As long as this card is attached to a Pokémon, it provides Fire Energy.\n\nThe Fire Pokémon this card is attached to gets +20 HP.",
-		fr: "Tant que cette carte est attachée à un Pokémon, elle fournit une Énergie Fire.\n\nCette carte ajoute 20 PV au Pokémon Fire auquel elle est attachée.",
+		fr: "Tant que cette carte est attachée à un Pokémon, elle fournit une Énergie Feu.",
 		es: "Mientras esta carta esté unida a 1 Pokémon, proporciona 1 Energía Fire.\n\nEl Pokémon Fire al que esté unida esta carta obtiene 20 PS más.",
 		it: "Fintanto che questa carta è assegnata a un Pokémon, fornisce Energia Fire.\n\nIl Pokémon Fire a cui è assegnata questa carta ha 20 PS in più.",
 		pt: "Enquanto esta carta estiver ligada a um Pokémon, ela fornecerá Energia Fire.\n\nO Pokémon Fire ao qual esta carta está ligada recebe 20 PS a mais.",
-		de: "Solange diese Karte an ein Pokémon angelegt ist, liefert sie Fire-Energie.\n\nDas Fire-Pokémon, an das diese Karte angelegt ist, erhält +20 KP."
+		de: "Solange diese Karte an ein Pokémon angelegt ist, liefert sie Fire-Energie.\n\nDas Fire-Pokémon, an das diese Karte angelegt ist, erhält +20 KP.",
 	},
 
 	energyType: "Special",
