06bfe747903e7719c47684834249452d4c38466f
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:14:04 +0100

    fix(data): update Sun & Moon / Unified Minds

--- data/Sun & Moon/Unified Minds/257.ts
+++ data/Sun & Moon/Unified Minds/257.ts
@@ -19,15 +19,15 @@ const card: Card = {
 
 	effect: {
 		en: "This card provides Colorless Energy.\n\nIf this card is discarded from play, put it into your hand instead of the discard pile.",
-		fr: "Cette carte fournit de l’Énergie Colorless.\n\nSi cette carte est défaussée du jeu, placez-la dans votre main plutôt que dans la pile de défausse.",
+		fr: "Si cette carte est défaussée du jeu, placez-la dans votre main plutôt que dans la pile de défausse.",
 		es: "Esta carta proporciona 1 Energía Colorless.\n\nSi se descarta esta carta del juego, ponla en tu mano en vez de en la pila de descartes.",
 		it: "Questa carta fornisce Energia Colorless.\n\nSe questa carta viene scartata dal gioco, aggiungila a quelle che hai in mano invece che alla pila degli scarti.",
 		pt: "Esta carta fornece Energia Colorless.\n\nSe esta carta for descartada do jogo, coloque-a na sua mão ao invés de colocá-la na pilha de descarte.",
-		de: "Diese Karte liefert Colorless-Energie.\n\nWenn diese Karte aus dem Spiel genommen wird und auf deinen Ablagestapel gelegt würde, nimm sie stattdessen auf deine Hand."
+		de: "Diese Karte liefert Colorless-Energie.\n\nWenn diese Karte aus dem Spiel genommen wird und auf deinen Ablagestapel gelegt würde, nimm sie stattdessen auf deine Hand.",
 	},
 
 	thirdParty: {
-		cardmarket: 389077,
+		cardmarket: 388847,
 		tcgplayer: 195332
 	}
 }
