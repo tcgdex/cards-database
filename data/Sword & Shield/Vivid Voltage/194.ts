37223f212cbf501f49d5ba3d50a65efea3c08c75
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:14:30 +0100

    fix(data): update Sword & Shield / Vivid Voltage

--- data/Sword & Shield/Vivid Voltage/194.ts
+++ data/Sword & Shield/Vivid Voltage/194.ts
@@ -18,12 +18,12 @@ const card: Card = {
 	category: "Trainer",
 
 	effect: {
-		fr: "Si vous jouez en premier, vous pouvez jouer cette carte pendant votre premier tour.\n\nPiochez 2 cartes.",
+		fr: "Si vous jouez en premier, vous pouvez jouer cette carte pendant votre premier tour.",
 		en: "If you go first, you may play this card during your first turn.\n\nDraw 2 cards.",
 		es: "Si sales primero, puedes jugar esta carta durante tu primer turno.\n\nRoba 2 cartas.",
 		it: "Se inizi per primo, puoi giocare questa carta durante il tuo primo turno.\n\nPesca due carte.",
 		pt: "Se você for primeiro, poderá jogar esta carta no seu primeiro turno.\n\nCompre 2 cartas.",
-		de: "Wenn du als Erster am Zug bist, kannst du diese Karte während deines ersten Zuges spielen.\n\nZiehe 2 Karten."
+		de: "Wenn du als Erster am Zug bist, kannst du diese Karte während deines ersten Zuges spielen.\n\nZiehe 2 Karten.",
 	},
 
 	trainerType: "Supporter",
@@ -37,7 +37,7 @@ const card: Card = {
 	},
 
 	thirdParty: {
-		cardmarket: 512680,
+		cardmarket: 512450,
 		tcgplayer: 226501
 	}
 }
