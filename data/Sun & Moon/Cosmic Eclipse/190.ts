05def8d8aaae10a563271d59af32c9fe880d0829
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:13:42 +0100

    fix(data): update Sun & Moon / Cosmic Eclipse

--- data/Sun & Moon/Cosmic Eclipse/190.ts
+++ data/Sun & Moon/Cosmic Eclipse/190.ts
@@ -16,20 +16,24 @@ const card: Card = {
 	category: "Trainer",
 	set: Set,
 
-	attacks: [{
-		name: {
-			en: 'Destructive Drake GX',
+	attacks: [
+		{
+			name: {
+				en: "Destructive Drake GX",
+				fr: "Destruction Draconique GX",
+			},
+			damage: "80×",
+			effect: {
+				en: "Discard all basic Energy from this Pokémon. This attack does 80 damage for each card you discarded in this way. (you can't use mote than 1 GX attack in a game.)",
+				fr: "Défaussez toute l'Énergie de base de ce Pokémon. Cette attaque inflige 80 dégâts pour chaque carte défaussée de cette façon. (Vous ne pouvez utiliser qu'une attaque GX par partie.)",
+			},
+			cost: [
+				"Colorless",
+				"Colorless",
+				"Colorless",
+			],
 		},
-		damage: '80×',
-		effect: {
-			en: 'Discard all basic Energy from this Pokémon. This attack does 80 damage for each card you discarded in this way. (you can\'t use mote than 1 GX attack in a game.)'
-		},
-		cost: [
-			'Colorless',
-			'Colorless',
-			'Colorless'
-		]
-	}],
+	],
 
 	effect: {
 		fr: "Si le Pokémon auquel cette carte est attachée a l’attaque Dracogriffe, il peut utiliser l’attaque GX sur cette carte. (Vous avez toujours besoin de l’Énergie nécessaire pour utiliser cette attaque.)\n\nColorlessColorlessColorless Destruction Draconique GX 80×\nDéfaussez toute l’Énergie de base de ce Pokémon. Cette attaque inflige 80 dégâts pour chaque carte défaussée de cette façon. (Vous ne pouvez utiliser qu’une attaque GX par partie.)",
