4066b1899b6f299f684e8c1ab53c38c6de195764
lamplis <lamplis@users.noreply.github.com>
2026-03-23 10:41:44 +0100

    fix(data): update EX / Ruby & Sapphire (#1213)

--- data/EX/Ruby & Sapphire/94.ts
+++ data/EX/Ruby & Sapphire/94.ts
@@ -15,7 +15,8 @@ const card: Card = {
 	energyType: "Special",
 
 	effect: {
-		de: "Schaden, der dem Pokémon, an das Metall-Energie angelegt ist, durch Angriffe zugefügt wird, wird um 10 reduziert (nachdem Schwäche und Resistenz verrechnet wurden). Dieser Effekt wirkt nur, wenn die Metall-Energie an einem Pokémon vom Typ  angelegt ist. Metall-Energie liefert -Energie. (Zählt nicht als Basis-Energiekarte.)"
+		de: "Schaden, der dem Pokémon, an das Metall-Energie angelegt ist, durch Angriffe zugefügt wird, wird um 10 reduziert (nachdem Schwäche und Resistenz verrechnet wurden). Dieser Effekt wirkt nur, wenn die Metall-Energie an einem Pokémon vom Typ  angelegt ist. Metall-Energie liefert -Energie. (Zählt nicht als Basis-Energiekarte.)",
+		fr: "Les dégâts infligés au Pokémon auquel Énergie Métal est attachée sont réduits de 10 (après application de la Faiblesse et de la Résistance). Ne tenez pas compte de cet effet si le Pokémon auquel Énergie Métal est attachée n'est pas Métal. Énergie Métal fournit une Énergie Métal (Elle ne compte pas comme Énergie de base).",
 	},
 
 	thirdParty: {
