c1708291800c15d52bf57d2ee88b7b9257fd1a9d
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:22 +0100

    fix(data): update McDonald's Collection / McDonald's Collection 2022

---
+++ data/McDonald's Collection/McDonald's Collection 2022/14.ts
@@ -0,0 +1,88 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2022'
+
+const card: Card = {
+	name: {
+		en: "Drampa",
+		fr: "Draïeul",
+		es: "Drampa",
+		it: "Drampa",
+		pt: "Drampa",
+		de: "Sen-Long"
+	},
+
+	illustrator: "Tomokazu Komiya",
+	rarity: "None",
+	category: "Pokemon",
+	set: Set,
+
+	dexId: [780],
+
+	hp: 120,
+
+	types: ["Colorless"],
+
+	stage: "Basic",
+
+	attacks: [
+		{
+			cost: ["Colorless"],
+			name: {
+				en: "Dragon Pulse",
+				fr: "Draco-Choc",
+				es: "Pulso Dragón",
+				it: "Dragopulsar",
+				pt: "Pulso do Dragão",
+				de: "Drachenpuls"
+			},
+			damage: 20,
+			effect: {
+				en: "Discard the top card of your deck.",
+				fr: "Défaussez la carte du dessus de votre deck.",
+				es: "Descarta la primera carta de tu baraja.",
+				it: "Scarta la prima carta del tuo mazzo.",
+				pt: "Descarte a carta de cima do seu baralho.",
+				de: "Lege die oberste Karte deines Decks auf deinen Ablagestapel."
+			},
+		},
+		{
+			cost: ["Colorless", "Colorless", "Colorless"],
+			name: {
+				en: "Power Cyclone",
+				fr: "Cyclone de Puissance",
+				es: "Ciclón de Poder",
+				it: "Ciclone di Potenza",
+				pt: "Ciclone de Poder",
+				de: "Kraftzyklon"
+			},
+			damage: 80,
+			effect: {
+				en: "Move an Energy from this Pokémon to 1 of your Benched Pokémon.",
+				fr: "Déplacez une Énergie de ce Pokémon vers l'un de vos Pokémon de Banc.",
+				es: "Mueve 1 Energía de este Pokémon a 1 de tus Pokémon en Banca.",
+				it: "Sposta un'Energia da questo Pokémon a uno dei tuoi Pokémon in panchina.",
+				pt: "Mova 1 Energia deste Pokémon para 1 dos seus Pokémon no Banco.",
+				de: "Verschiebe 1 Energie von diesem Pokémon auf 1 Pokémon auf deiner Bank."
+			},
+		},
+	],
+
+	weaknesses: [
+		{
+			type: "Fighting",
+			value: "×2"
+		},
+	],
+
+	retreat: 2,
+
+	variants: {
+		normal: true,
+		reverse: false,
+		holo: true,
+		firstEdition: false
+	}
+}
+
+export default card
+
---
+++ data/McDonald's Collection/McDonald's Collection 2022/15.ts
@@ -0,0 +1,79 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2022'
+
+const card: Card = {
+	name: {
+		en: "Smeargle",
+		fr: "Queulorior",
+		es: "Smeargle",
+		it: "Smeargle",
+		pt: "Smeargle",
+		de: "Farbeagle"
+	},
+
+	illustrator: "Kouki Saitou",
+	rarity: "None",
+	category: "Pokemon",
+	set: Set,
+
+	dexId: [235],
+
+	hp: 70,
+
+	types: ["Colorless"],
+
+	stage: "Basic",
+
+	attacks: [
+		{
+			cost: ["Colorless"],
+			name: {
+				en: "Sketch",
+				fr: "Gribouille",
+				es: "Esquema",
+				it: "Schizzo",
+				pt: "Esboço",
+				de: "Nachahmer"
+			},
+			effect: {
+				en: "Draw a card.",
+				fr: "Piochez une carte.",
+				es: "Roba 1 carta.",
+				it: "Pesca una carta.",
+				pt: "Compre 1 carta.",
+				de: "Ziehe 1 Karte."
+			},
+		},
+		{
+			cost: ["Colorless", "Colorless"],
+			name: {
+				en: "Tail Smack",
+				fr: "Coup de Queue",
+				es: "Golpe de Cola",
+				it: "Codacolpo",
+				pt: "Golpe de Cauda",
+				de: "Schwanzschlag"
+			},
+			damage: 30,
+		},
+	],
+
+	weaknesses: [
+		{
+			type: "Fighting",
+			value: "×2"
+		},
+	],
+
+	retreat: 1,
+
+	variants: {
+		normal: true,
+		reverse: false,
+		holo: true,
+		firstEdition: false
+	}
+}
+
+export default card
+
