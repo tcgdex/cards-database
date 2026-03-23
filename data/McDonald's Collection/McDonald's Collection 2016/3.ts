9e90736e84c69b337da7571b0d6e3ff127d6ea65
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:12:08 +0100

    fix(data): update McDonald's Collection / McDonald's Collection 2016

---
+++ data/McDonald's Collection/McDonald's Collection 2016/3.ts
@@ -0,0 +1,40 @@
+import { Card } from '../../../interfaces'
+import Set from '../McDonald\'s Collection 2016'
+
+const card: Card = {
+	set: Set,
+	illustrator: "5ban Graphics",
+	category: "Pokemon",
+
+	dexId: [653],
+
+	description: {
+		en: "Eating a twig fills it with energy, and its roomy ears give vent to air hotter than 390 degrees Fahrenheit."
+	},
+	
+	hp: 50,
+
+	stage: "Basic",
+
+	attacks: [{
+		name: {
+			en: "Will-O-Wisp",
+			fr: "Feu Follet"
+		},
+
+		damage: 20
+	}],
+
+	name: {
+		en: "Fennekin",
+		fr: "Feunnec"
+	},
+
+	rarity: "None",
+
+	thirdParty: {
+		tcgplayer: 275059
+	}
+}
+
+export default card
