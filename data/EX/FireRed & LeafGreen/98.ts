f5f5fc2b6c72b4b436d0176d0ee678d938c49774
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:11:20 +0100

    fix(data): update EX / FireRed & LeafGreen

--- data/EX/FireRed & LeafGreen/98.ts
+++ data/EX/FireRed & LeafGreen/98.ts
@@ -15,8 +15,8 @@ const card: Card = {
 	trainerType: "Supporter",
 
 	effect: {
-		fr: "Vous ne pouvez jouer qu'une seule carte Supporter par tour. Lorsque vous jouez cette carte, placez-la à côté de votre Pokémon Actif. À la fin de votre tour, défaussez-la.\n\nMélangez votre main avec votre deck. Ensuite, piochez 5 cartes.",
-		de: "Mische deine Hand in dein Deck, und ziehe dann 5 Karten."
+		fr: "Mélangez votre main avec votre deck. Ensuite, piochez 5 cartes.",
+		de: "Mische deine Hand in dein Deck, und ziehe dann 5 Karten.",
 	},
 
 	thirdParty: {
