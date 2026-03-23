799fb0958a100b5cddc14b559142aa60a0400b9a
Laurent AMPLIS <lamplis@yahoo.fr>
2026-03-23 09:13:28 +0100

    fix(data): update Scarlet & Violet / Prismatic Evolutions

--- data/Scarlet & Violet/Prismatic Evolutions/106.ts
+++ data/Scarlet & Violet/Prismatic Evolutions/106.ts
@@ -18,11 +18,11 @@ const card: Card = {
 
 	effect: {
 		en: "You can use this card only if you discard another card from your hand.\n\nSearch your deck for up to 2 Basic Energy cards, reveal them, and put them into your hand. Then, shuffle your deck.",
-		fr: "Vous ne pouvez utiliser cette carte que si vous défaussez une autre carte de votre main.\n\nCherchez dans votre deck jusqu'à 2 cartes Énergie de base, montrez-les, puis ajoutez-les à votre main. Mélangez ensuite votre deck.",
+		fr: "Vous ne pouvez utiliser cette carte que si vous défaussez une autre carte de votre main.",
 		es: "Puedes usar esta carta solo si descartas otra carta de tu mano.\n\nBusca en tu baraja hasta 2 cartas de Energía Básica, enséñalas y ponlas en tu mano. Después, baraja las cartas de tu baraja.",
 		pt: "Você só pode usar esta carta se descartar outra carta da sua mão.\n\nProcure por até 2 cartas de Energia Básica no seu baralho, revele-as e coloque-as na sua mão. Em seguida, embaralhe o seu baralho.",
 		it: "Puoi usare questa carta solo se scarti un'altra carta che hai in mano.\n\nCerca nel tuo mazzo fino a due carte Energia base, mostrale e aggiungile alle carte che hai in mano. Poi rimischia le carte del tuo mazzo.",
-		de: "Du kannst diese Karte nur einsetzen, wenn du noch 1 andere Karte aus deiner Hand auf deinen Ablagestapel legst.\n\nDurchsuche dein Deck nach bis zu 2 Basis-Energiekarten, zeige sie deinem Gegner und nimm sie auf deine Hand. Mische anschließend dein Deck."
+		de: "Du kannst diese Karte nur einsetzen, wenn du noch 1 andere Karte aus deiner Hand auf deinen Ablagestapel legst.\n\nDurchsuche dein Deck nach bis zu 2 Basis-Energiekarten, zeige sie deinem Gegner und nimm sie auf deine Hand. Mische anschließend dein Deck.",
 	},
 
 	trainerType: "Item",
--- data/Scarlet & Violet/Prismatic Evolutions/134.ts
+++ data/Scarlet & Violet/Prismatic Evolutions/134.ts
@@ -18,11 +18,11 @@ const card: Card = {
 
 	effect: {
 		en: "You can use this card only if your opponent's Active Pokémon is Poisoned.\n\nShuffle your hand into your deck. Then, draw 7 cards.",
-		fr: "Vous ne pouvez utiliser cette carte que si le Pokémon Actif de votre adversaire est Empoisonné.\n\nMélangez votre main avec votre deck. Ensuite, piochez 7 cartes.",
+		fr: "Vous ne pouvez utiliser cette carte que si le Pokémon Actif de votre adversaire est Empoisonné.",
 		es: "Puedes usar esta carta solo si el Pokémon Activo de tu rival está Envenenado.\n\nPon las cartas de tu mano en tu baraja y barájalas todas. Después, roba 7 cartas.",
 		pt: "Você só pode usar esta carta se o Pokémon Ativo do seu oponente estiver Envenenado.\n\nEmbaralhe a sua mão no seu baralho. Em seguida, compre 7 cartas.",
 		it: "Puoi usare questa carta solo se il Pokémon attivo del tuo avversario è avvelenato.\n\nRimischia le carte che hai in mano nel tuo mazzo. Poi pesca sette carte.",
-		de: "Du kannst diese Karte nur einsetzen, wenn das Aktive Pokémon deines Gegners vergiftet ist.\n\nMische deine Handkarten in dein Deck. Ziehe anschließend 7 Karten."
+		de: "Du kannst diese Karte nur einsetzen, wenn das Aktive Pokémon deines Gegners vergiftet ist.\n\nMische deine Handkarten in dein Deck. Ziehe anschließend 7 Karten.",
 	},
 
 	trainerType: "Supporter",
