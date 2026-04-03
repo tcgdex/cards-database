22d7975fd29010ac4419c159661897788a643dc3
lamplis <lamplis@users.noreply.github.com>
2026-04-03 14:16:29 +0200

    fix(data): add missing dexId for tcgp fantastical parade (#1352)
    Populate missing dexId fields for Fantastical Parade cards using the pokedexIdFixer mappings and normalized species matching.

--- data/Pokémon TCG Pocket/Fantastical Parade/066.ts
+++ data/Pokémon TCG Pocket/Fantastical Parade/066.ts
@@ -4,6 +4,8 @@ import Set from "../Fantastical Parade"
 const card: Card = {
 	set: Set,
 
+	dexId: [282],
+
 	name: {
 		en: "Mega Gardevoir ex"
 	},
--- data/Pokémon TCG Pocket/Fantastical Parade/086.ts
+++ data/Pokémon TCG Pocket/Fantastical Parade/086.ts
@@ -4,6 +4,8 @@ import Set from "../Fantastical Parade"
 const card: Card = {
 	set: Set,
 
+	dexId: [525],
+
 	name: {
 		en: "Boldore"
 	},
