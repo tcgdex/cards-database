84cae068356e4b7b9ba2525bf0bb88c5bf409e1b
FalconChipp <112320693+FalconChipp@users.noreply.github.com>
2026-04-14 23:21:19 +0100

    Use 'stamp' instead of 'foil' for holo prints
    Replace the 'foil' property with 'stamp' for two holo variants (regular and jumbo) - jtg-030

--- data/Scarlet & Violet/Journey Together/030.ts
+++ data/Scarlet & Violet/Journey Together/030.ts
@@ -75,14 +75,14 @@ const card: Card = {
 		},
 		{
 			type: "holo",
-			foil: ["set-logo"],
+			stamp: ["set-logo"],
 			thirdParty: {
 				cardmarket: 841282,
 			}
 		},
 		{
 			type: "holo",
-			foil: ["set-logo"],
+			stamp: ["set-logo"],
 			size: "jumbo",
 			thirdParty: {
 				cardmarket: 841283,
