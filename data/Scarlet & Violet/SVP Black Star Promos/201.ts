a01847756114fa4be4e0ad5b80c460c6e5b38d30
andersonpss <anderson.pss@outlook.com>
2026-04-17 20:52:37 -0400

    Added iinformation in other languages

--- data/Scarlet & Violet/SVP Black Star Promos/201.ts
+++ data/Scarlet & Violet/SVP Black Star Promos/201.ts
@@ -6,53 +6,69 @@ const card: Card = {
 
 	name: {
 		en: "Zebstrika",
-		fr: "Zéblitz"
+		fr: "Zéblitz",
+		de: "Zebritz",
+		it: "Zebstrika",
+		es: "Zebstrika",
+		pt: "Zebstrika"
 	},
 
+	illustrator: "Krgc",
 	rarity: "None",
 	category: "Pokemon",
+	dexId: [523],
 	hp: 120,
 	types: ["Lightning"],
+	stage: "Stage1",
+
 	evolveFrom: {
 		en: "Blitzle",
 		fr: "Zébibron",
 		de: "Elezeba",
 		it: "Blitzle",
-		pt: "Blitzle",
 		es: "Blitzle",
-		'es-mx': "Blitzle"
+		pt: "Blitzle"
 	},
-	stage: "Stage1",
-	illustrator: "Krgc",
-	dexId: [523],
-	attacks: [
-		{
-			cost: ["Colorless"],
-			name: {
-				en: "Full Speed",
-				fr: "Pleine Vitesse"
-			},
-			effect: {
-				en: "Discard your hand and draw 6 cards.",
-				fr: "Défaussez votre main, puis piochez 6 cartes."
-			}
-		},
-		{
-			cost: ["Lightning", "Colorless"],
-			name: {
-				en: "Head Bolt",
-				fr: "Éclair Frontal"
-			},
-			damage: 70,
-		}
-	],
 
-	weaknesses: [
-		{
-			type: "Fighting",
-			value: "×2"
-		}
-	],
+	attacks: [{
+		cost: ["Colorless"],
+
+		name: {
+			en: "Full Speed",
+			fr: "Pleine Vitesse",
+			de: "Full Speed",
+			it: "Piena Velocità",
+			es: "A Toda Velocidad",
+			pt: "Velocidade Máxima"
+	},
+
+		effect: {
+			en: "Discard your hand and draw 6 cards.",
+			fr: "Défaussez votre main, puis piochez 6 cartes.",
+			de: "Lege deine Handkarten auf deinen Ablagestapel und ziehe 6 Karten.",
+			it: "Scarta le carte che hai in mano e pesca sei carte.",
+			es: "Descarta las cartas de tu mano y roba 6 cartas.",
+			pt: "Descarte a sua mão e compre 6 cartas."
+	}
+	}, {
+		cost: ["Lightning", "Colorless"],
+
+		name: {
+			en: "Head Bolt",
+			fr: "Éclair Frontal",
+			de: "Kopf-Blitz",
+			it: "Zuccalampo",
+			es: "Rayo de Cabeza",
+			pt: "Raio de Cabeça"
+	},
+
+		damage: 70
+	}],
+
+	weaknesses: [{
+		type: "Fighting",
+		value: "x2"
+	}],
 
 	retreat: 1,
 	regulationMark: "H",
--- data/Scarlet & Violet/SVP Black Star Promos/211.ts
+++ data/Scarlet & Violet/SVP Black Star Promos/211.ts
@@ -2,74 +2,103 @@ import { Card } from "../../../interfaces"
 import Set from "../SVP Black Star Promos"
 
 const card: Card = {
-	dexId: [576],
 	set: Set,
 
 	name: {
 		en: "Gothitelle",
-		fr: "Sidérella"
+		fr: "Sidérella",
+		de: "Morbitesse",
+		it: "Gothitelle",
+		es: "Gothitelle",
+		pt: "Gothitelle"
 	},
 
+	illustrator: "IKEDA Saki",
 	rarity: "None",
 	category: "Pokemon",
+	dexId: [576],
 	hp: 150,
-	types: ["Colorless"],
+	types: ["Psychic"],
+	stage: "Stage2",
+
 	evolveFrom: {
 		en: "Gothorita",
 		fr: "Mesmérella",
 		de: "Hypnomorba",
 		it: "Gothorita",
-		pt: "Gothorita",
 		es: "Gothorita",
-		'es-mx': "Gothorita"
+		pt: "Gothorita"
 	},
-	stage: "Stage2",
-	illustrator: "IKEDA Saki",
 
-	attacks: [
-		{
-			cost: ["Psychic","Colorless"],
+	abilities: [{
+		type: "Ability",
 
-			name: {
-				en: "Synchro Shot",
-				fr: "Coup Synchro"
-			},
+		name: {
+			en: "Distorted Future",
+			fr: "Futur Déformé",
+			de: "Verzerrte Zukunft",
+			it: "Futuro Distorto",
+			es: "Futuro Distorsionado",
+			pt: "Futuro Distorcido"
+	},
 
-			effect: {
-				en: "If you have the same number of cards in your hand as your opponent, this attack does 90 more damage.",
-				fr: "Si vous avez le même nombre de cartes dans votre main que votre adversaire, cette attaque inflige 90 dégâts supplémentaires."
-			},
-			damage: "90+"
-		}
-	],
+		effect: {
+			en: "Once during your turn, if this Pokémon is in the Active Spot, you may have your opponent shuffle their hand into their deck and draw 3 cards.",
+			fr: "Une fois pendant votre tour, si ce Pokémon est sur le Poste Actif, vous pouvez demander à votre adversaire de mélanger sa main avec son deck et de piocher 3 cartes.",
+			de: "Einmal während deines Zuges, wenn dieses Pokémon in der Aktiven Position ist, kannst du deinen Gegner dazu veranlassen, seine Handkarten in sein Deck zu mischen und 3 Karten zu ziehen.",
+			it: "Una sola volta durante il tuo turno, se questo Pokémon è in posizione attiva, puoi far rimischiare al tuo avversario le carte che ha in mano nel suo mazzo e fargli pescare tre carte.",
+			es: "Una vez durante tu turno, si este Pokémon está en el Puesto Activo, puedes hacer que tu rival ponga las cartas de su mano en su baraja, las baraje todas y robe 3 cartas.",
+			pt: "Uma vez durante o seu turno, se este Pokémon estiver no Campo Ativo, você poderá fazer com que o seu oponente embaralhe a mão dele no baralho dele e compre 3 cartas."
+	}
+	}],
 
-	weaknesses: [
-		{
-			type: "Darkness",
-			value: "×2"
-		}
-	],
+	attacks: [{
+		cost: ["Psychic", "Colorless"],
 
-	resistances: [
-		{
-			type: "Fighting",
-			value: "-30"
-		}
-	],
+		name: {
+			en: "Synchro Shot",
+			fr: "Coup Synchro",
+			de: "Synchro-Schuss",
+			it: "Sincrocolpo",
+			es: "Disparo Sincronizado",
+			pt: "Disparo Sincronizado"
+	},
+
+		effect: {
+			en: "If you have the same number of cards in your hand as your opponent, this attack does 90 more damage.",
+			fr: "Si vous avez le même nombre de cartes dans votre main que votre adversaire, cette attaque inflige 90 dégâts supplémentaires.",
+			de: "Wenn du genauso viele Karten auf der Hand hast wie dein Gegner, fügt diese Attacke 90 Schadenspunkte mehr zu.",
+			it: "Se hai lo stesso numero di carte in mano del tuo avversario, questo attacco infligge 90 danni in più.",
+			es: "Si tienes la misma cantidad de cartas en tu mano que tu rival, este ataque hace 90 puntos de daño más.",
+			pt: "Se você tiver o mesmo número de cartas na sua mão do que seu oponente, este ataque causará 90 pontos de dano a mais."
+	},
+
+		damage: "90+"
+	}],
+
+	weaknesses: [{
+		type: "Darkness",
+		value: "x2"
+	}],
+
+	resistances: [{
+		type: "Fighting",
+		value: "-30"
+	}],
 
 	retreat: 2,
 	regulationMark: "I",
+
+	thirdParty: {
+		cardmarket: 836659,
+		tcgplayer: 647306
+	},
 
 	variants: [
 		{
 			type: "holo"
 		},
 	],
-
-	thirdParty: {
-		cardmarket: 836659,
-		tcgplayer: 647306,
-	}
 }
 
 export default card
