7ed3c5c1567d2583e83039a404ab2d9237fbb0ed
andersonpss <anderson.pss@outlook.com>
2026-04-26 10:08:06 -0400

    Add missing SM Black Star Promos

---
+++ data/Sun & Moon/SM Black Star Promos/SM246.ts
@@ -0,0 +1,36 @@
+import { Card } from '../../../interfaces'
+import Set from '../SM Black Star Promos'
+
+const card: Card = {
+	name: {
+		en: "Sabrina & Brycen",
+		fr: "Morgane et Zhu",
+		es: "Sabrina y Junco",
+		it: "Sabrina e Silvestro",
+		pt: "Sabrina e Brycen",
+		de: "Sabrina & Sandro"
+	},
+
+	illustrator: "Ryuta Fuse",
+	rarity: "Uncommon",
+	category: "Trainer",
+
+	set: Set,
+
+	effect: {
+		en: "Search your deck for up to 2 basic Energy cards, reveal them, and put them into your hand. Then, shuffle your deck.\n\nWhen you play this card, you may discard 5 other cards from your hand. If you do, you may also search for up to 3 Pokémon of different types in this way.",
+		fr: "Cherchez dans votre deck jusqu'à 2 cartes Énergie de base, montrez-les, puis ajoutez-les à votre main. Mélangez ensuite votre deck.\n\nLorsque vous jouez cette carte, vous pouvez défausser 5 autres cartes de votre main. Dans ce cas, vous pouvez aussi chercher jusqu'à 3 Pokémon de types différents de cette façon.",
+		es: "Busca en tu baraja hasta 2 cartas de Energía Básica, enséñalas y ponlas en tu mano. Después, baraja las cartas de tu baraja.\n\nCuando juegues esta carta, puedes descartar otras 5 cartas de tu mano. Si lo haces, también puedes buscar hasta 3 Pokémon de diferentes tipos de esta manera.",
+		it: "Cerca nel tuo mazzo fino a 2 carte Energia base, mostrale e aggiungile alle carte che hai in mano. Poi rimischia le carte del tuo mazzo.\n\nQuando giochi questa carta, puoi scartare altre 5 carte che hai in mano. Se lo fai, puoi anche cercare fino a 3 Pokémon di tipi diversi in questo modo.",
+		pt: "Procure por até 2 cartas de Energia básica no seu baralho, revele-as e coloque-as na sua mão. Em seguida, embaralhe o seu baralho.\n\nQuando você jogar este card, você poderá descartar 5 outras cartas da sua mão. Se fizer isto, você também poderá procurar por até 3 Pokémon de tipos diferentes desta forma.",
+		de: "Durchsuche dein Deck nach bis zu 2 Basis-Energiekarten, zeige sie deinem Gegner und nimm sie auf deine Hand. Mische anschließend dein Deck.\n\nWenn du diese Karte spielst, kannst du 5 andere Karten aus deiner Hand auf deinen Ablagestapel legen. Wenn du das machst, kannst du auf diese Weise auch nach bis zu 3 Pokémon unterschiedlichen Typs suchen."
+	},
+
+	trainerType: "Supporter",
+
+	thirdParty: {
+		tcgplayer: 245873
+	}
+}
+
+export default card
