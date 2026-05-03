7ab6ebb5c22fd418d92b2e734f39717bf90e594e
andersonpss <anderson.pss@outlook.com>
2026-05-03 19:33:11 -0400

    Add zn-ch  CSMPiC Cards to Data-Asia

---
+++ data-asia/SM/CSMPiC/032.ts
@@ -0,0 +1,19 @@
+import { Card } from "../../../interfaces"
+import Set from "../CSMPiC"
+
+const card: Card = {
+	set: Set,
+	name: {
+		'zh-cn': "青绿的策略",
+	},
+	rarity: "None",
+	illustrator: "Naoki Saito",
+	category: "Trainer",
+	effect: {
+		'zh-cn': "在这个回合结束时，抽卡直到自己的手牌变为8张。",
+	},
+	trainerType: "Supporter",
+	variants: [{ type: "holo" }],
+}
+
+export default card
