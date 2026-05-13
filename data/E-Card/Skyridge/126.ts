6bc24d3d5dd742d4316b0ab97764cf008bb1bb75
Phil Gruneich <github@onetapless.com>
2026-05-12 23:37:15 -0400

    patch: EN data E-Card/Skyridge

--- data/E-Card/Skyridge/126.ts
+++ data/E-Card/Skyridge/126.ts
@@ -13,6 +13,7 @@ const card: Card = {
 	set: Set,
 
 	effect: {
+		en: "Choose 1 of your opponent's Pokémon. Search your deck for a Baby Pokémon, Basic Pokémon, or Evolution card of the same type (color), show it to your opponent, and put it into your hand. Shuffle your deck afterward.",
 		de: "Wähle 1 der Pokémon deines Gegners. Durchsuche dein Deck nach einem Baby-Pokémon, einem Basis-Pokémon oder einer Entwicklungskarte des gleichen Typs (der gleichen Farbe), zeige diese Karte deinem Gegner und nimm sie auf deine Hand. Mische dein Deck danach."
 	},
 
