50bb734fa5cd99cae68908f23a01a3ab1364242d
Phillip Gruneich <philgruneich@users.noreply.github.com>
2026-05-13 04:47:12 -0400

    patch: EN data E-Card/Skyridge (#1447)

--- data/E-Card/Skyridge/124.ts
+++ data/E-Card/Skyridge/124.ts
@@ -13,6 +13,7 @@ const card: Card = {
 	set: Set,
 
 	effect: {
+		en: "Reveal cards from your deck until you reveal an Evolution card. Show that card to your opponent and put it into your hand. Shuffle the other revealed cards into your deck. (If you don't reveal an Evolution card, shuffle all the revealed cards back into your deck.)",
 		de: "Decke Karten aus deinem deck auf, bis du eine Entwicklungskarte aufdeckst. Zeige diese Karte deinem Gegner und nimm sie auf deine Hand. Mische die anderen aufgedeckten Karten in dein Deck. (Ist unter den aufgedeckten Karten keine Entwicklungskarte, mische alle aufgedeckten karten zurück in dein Deck.)"
 	},
 
