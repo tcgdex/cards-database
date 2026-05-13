1e0a0f4ad1151282c949a46993fe762729bd0fd8
Phillip Gruneich <philgruneich@users.noreply.github.com>
2026-05-13 04:36:17 -0400

    patch: EN data Gym/Gym Heroes (#1438)

--- data/Gym/Gym Heroes/113.ts
+++ data/Gym/Gym Heroes/113.ts
@@ -11,6 +11,9 @@ const card: Card = {
 	category: "Trainer",
 	set: Set,
 
+	effect: {
+		en: "Flip 2 coins. If both of them are heads, choose 1 of your opponent's Benched Pokémon and return it and all cards attached to it to his or her hand. If 1 or both of them are tails, your turn ends immediately (you can't attack this turn).",
+	},
 	thirdParty: {
 		cardmarket: 274249,
 		tcgplayer: 87480
--- data/Gym/Gym Heroes/18.ts
+++ data/Gym/Gym Heroes/18.ts
@@ -11,6 +11,9 @@ const card: Card = {
 	category: "Trainer",
 	set: Set,
 
+	effect: {
+		en: "Discard 2 of the other cards in your hand in order to play this card. If this turn's attack does damage to the Defending Pokémon (after applying Weakness and Resistance), and if the attacking Pokémon has Misty in its name, the attack does 20 more damage to the Defending Pokémon.",
+	},
 	thirdParty: {
 		cardmarket: 274154,
 		tcgplayer: 87523
