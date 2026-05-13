176d53b44930f49cf4735fd35bfca1cafefee920
Phil Gruneich <github@onetapless.com>
2026-05-12 23:35:51 -0400

    patch: EN data E-Card/Expedition Base Set

--- data/E-Card/Expedition Base Set/152.ts
+++ data/E-Card/Expedition Base Set/152.ts
@@ -14,6 +14,7 @@ const card: Card = {
 	set: Set,
 
 	effect: {
+		en: "Your opponent switches the Defending Pokémon with 1 of his or her Benched Pokémon, if any; then you switch your Active Pokémon with 1 of your Benched Pokémon, if any.",
 		fr: "Votre adversaire échange le Pokémon Défenseur contre l'un des Pokémon de son Banc, s'il en possède. Vous échangez ensuite votre Pokémon Actif contre l'un des Pokémon de votre Banc, si vous en possédez.",
 		de: "Dein Gegner tauscht das Verteidigende Pokémon mit 1 der Pokémon auf seiner Bank, falls er dort mindestens eins hat, und danach tauscht du dein Aktives Pokémon mit 1 der Pokémon auf deiner Bank, falls du dort mindestens eins hast."
 	},
