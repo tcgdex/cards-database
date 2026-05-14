6229f40687cb2a07695553e33d74eb7e6ba78a7d
Phil Gruneich <github@onetapless.com>
2026-05-13 21:48:35 -0400

    patch: EN data EX/Unseen Forces

--- data/EX/Unseen Forces/84.ts
+++ data/EX/Unseen Forces/84.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Item",
 
 	effect: {
+		en: "Move a basic Energy card attached to 1 of your Pokémon to another of your Pokémon.",
 		fr: "Déplacez une carte Énergie de base attachée à 1 de vos Pokémon sur un autre de vos Pokémon.",
 		de: "Move a basic Energy card attached to 1 of your Pokémon to another of your Pokémon."
 	},
