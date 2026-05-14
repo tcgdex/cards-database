c9f6a7bc518521effe70b535a00f0bef9cafa6b5
Phil Gruneich <github@onetapless.com>
2026-05-13 21:39:40 -0400

    patch: EN data EX/Holon Phantoms

--- data/EX/Holon Phantoms/97.ts
+++ data/EX/Holon Phantoms/97.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	energyType: "Special",
 
 	effect: {
+		en: "Attach Dark Metal Energy to 1 of your Pokémon. While in play, Dark Metal Energy provides Darkness Energy and Metal Energy, but provides only 1 Energy at a time. (Doesn't count as a basic Energy card when not in play and has no effect other than providing Energy.)",
 		de: "Lege Finstere Metall-Energie an 1 deiner Pokémon an. Während Finstere Metall-Energie im Spiel ist, liefert diese Karte - und -Energie, aber immer nur 1 Energie auf einmal. (Zählt nicht als Basis-Energiekarte, wenn sie nicht im Spiel ist und hat keinen Effekt, außer dass sie Energie spendet.)",
 		fr: "Attachez Énergie Métal Obscur à 1 de vos Pokémon. Lorsqu'elle est en jeu, Énergie Métal Obscur fournit les types d'énergie Obscurité et Métal (mais seulement un à la fois). (Elle ne compte pas comme carte Énergie de base lorsqu'elle n'est pas en jeu et n'a pas d'autre effet que de fournir de l'Énergie.)",
 	},
