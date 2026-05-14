7e98ac0442f1c2b929be475f4e4acbe1dc74c473
Phil Gruneich <github@onetapless.com>
2026-05-13 21:50:39 -0400

    patch: EN data EX/FireRed & LeafGreen

--- data/EX/FireRed & LeafGreen/99.ts
+++ data/EX/FireRed & LeafGreen/99.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Item",
 
 	effect: {
+		en: "Flip a coin. If heads, return 1 of your Pokémon and all cards attached to it to your hand.",
 		fr: "Lancez une pièce. Si c'est face, reprenez dans votre main 1 de vos Pokémon ainsi que toutes les cartes qui lui sont attachées.",
 		de: "Wirf 1 Münze. Nimm bei \"Kopf\" 1 deiner Pokémon und alle daran angelegten Karten zurück auf die Hand."
 	},
