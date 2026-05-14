4e9477ef495f4ef929b2cecf3467edeb59f37d06
Phil Gruneich <github@onetapless.com>
2026-05-13 21:50:16 -0400

    patch: EN data EX/Team Rocket Returns

--- data/EX/Team Rocket Returns/94.ts
+++ data/EX/Team Rocket Returns/94.ts
@@ -14,6 +14,7 @@ const card: Card = {
 	energyType: "Special",
 
 	effect: {
+		en: "Attach Dark Metal Energy to 1 of your Pokémon. While in play, Dark Metal Energy provides Darkness Energy and Metal Energy, but provides only 1 Energy at a time. (Doesn't count as a basic Energy card when not in play and has no effect other than providing Energy.)",
 		de: "Lege die Dunkle Metall Energie an eins deiner Pokémon an. Die Dunkle Metall Energie liefert 1 Energie und 1 Energie, solange sie im Spiel ist, liefert aber nur eine Energie auf einmal. (Zählt nicht als Basisenergiekarte und hat außerhalb des Spiels keinen anderen Effekt, als Energie zu liefern.)"
 	},
 
