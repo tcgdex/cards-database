b88a4791ff04928a43da80c4ace4787f4446851b
Phil Gruneich <github@onetapless.com>
2026-05-13 21:49:25 -0400

    patch: EN data EX/Deoxys

--- data/EX/Deoxys/90.ts
+++ data/EX/Deoxys/90.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Supporter",
 
 	effect: {
+		en: "You can play only one Supporter card each turn. When you play this card, put it next to your Active Pokémon. When your turn ends, discard this card. Flip a coin. If heads, draw the bottom 3 cards of your deck. If tails, draw the top 2 cards of your deck.",
 		fr: "Lancez une pièce. Si c'est face, piochez les 3 dernières cartes de votre deck. Si c'est pile, piochez les 2 cartes du dessus de votre deck.",
 		de: "Flip a coin. If heads, draw the bottom 3 cards of your deck. If tails, draw the top2 cards of your deck.",
 	},
