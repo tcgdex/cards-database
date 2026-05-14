67b4cf76956a84aa208402220d7f664b03c16322
Phil Gruneich <github@onetapless.com>
2026-05-13 21:51:23 -0400

    patch: EN data EX/Team Magma vs Team Aqua

--- data/EX/Team Magma vs Team Aqua/73.ts
+++ data/EX/Team Magma vs Team Aqua/73.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Supporter",
 
 	effect: {
+		en: "You can play only one Supporter card each turn. When you play this card, put it next to your Active Pokémon. When your turn ends, discard this card. Search your hand or discard pile for a Pokémon with Team Magma in its name and put it onto your Bench. Treat the new Benched Pokémon as a Basic Pokémon. If it is a Stage 2 Pokémon, put 2 damage counters on that Pokémon.",
 		fr: "Cherchez dans votre main ou votre pile de défausse un Pokémon dont le nom comporte Team Magma et placez-le sur votre Banc. Traitez le nouveau Pokémon de Banc comme un Pokémon de base. Si c'est un Pokémon de niveau 2, placez 2 marqueurs de dégât sur ce Pokémon.",
 		de: "Durchsuche dein Deck nach einem Pokémon mit",
 	},
--- data/EX/Team Magma vs Team Aqua/74.ts
+++ data/EX/Team Magma vs Team Aqua/74.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Tool",
 
 	effect: {
+		en: "Attach Strength Charm to 1 of your Pokémon that doesn't already have a Pokémon Tool attached to it. If that Pokémon is Knocked Out, discard this card. Whenever an attack from the Pokémon that Strength Charm is attached to does damage to the Active Pokémon (after applying Weakness and Resistance), the attack does 10 more damage. At the end of the turn in which this happens, discard Strength Charm.",
 		fr: "Attachez Sort de force à 1 de vos Pokémon qui ne possède pas déjà d'Outil Pokémon. Si ce Pokémon est mis KO, défaussez cette carte.\n\nDès qu'une attaque du Pokémon auquel est attaché Sort de force inflige des dégâts au Pokémon Actif, (avant application de la Faiblesse et de la Résistance), l'attaque inflige 10 dégâts supplémentaires. À la fin du tour au cours duquel l'attaque a été utilisée, défaussez Sort de force.",
 		de: "Immer wenn ein Angriff des Pokémons, an das Stärkezauber angelegt ist, dem verteidigenden Pokémon Schaden zufügt (nachdem Schwäche und Resistenz verrechnet werden), fügt die Attacke 10 Schadenspunkte mehr zu. Am Ende eines solchen Zuges, lege diese Karte ab."
 	},
