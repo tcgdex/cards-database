8c29df7b27d3603ba53b165c1cbe430d11ae538e
Phillip Gruneich <philgruneich@users.noreply.github.com>
2026-05-18 08:17:59 -0400

    patch: EN data EX/Sandstorm (#1453)

--- data/EX/Sandstorm/87.ts
+++ data/EX/Sandstorm/87.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Supporter",
 
 	effect: {
+		en: "You can play only one Supporter card each turn. When you play this card, put it next to your Active Pokémon. When your turn ends, discard this card. Search your deck for up to 3 different types of Basic Pokémon cards (excluding Baby Pokémon), show them to your opponent, and put them into your hand. Shuffle your deck afterward.",
 		fr: "Choisissez dans votre deck jusqu'à trois types de cartes Pokémon de base différents (sauf les cartes Bébé Pokémon), montrez-les à votre adversaire et placez-les dans votre main. Ensuite, mélangez votre deck.",
 		de: "Durchsuche dein Deck nach bis zu 3 verschiedenen Basis-Pokémon (außer Baby-Pokémon), zeige sie deinem Gegner und nimm sie auf die Hand. Mische dein Deck danach.",
 	},
