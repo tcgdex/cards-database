32a0ab27127ce42341285d998fea3274c5b88ac2
Phillip Gruneich <philgruneich@users.noreply.github.com>
2026-05-18 08:35:21 -0400

    patch: EN data EX/Unseen Forces (#1459)

--- data/EX/Unseen Forces/90.ts
+++ data/EX/Unseen Forces/90.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Tool",
 
 	effect: {
+		en: "Attach Protective Orb to 1 of your Evolved Pokémon (excluding Pokémon-ex) that doesn't already have a Pokémon Tool attached to it. If the Pokémon Protective Orb is attached to is a Basic Pokémon or Pokémon-ex, discard Protective Orb. As long as Protective Orb is attached to a Pokémon, that Pokémon has no Weakness.",
 		fr: "Attachez Sphère Protectrice à 1 de vos Pokémon Évolués (Pokémon-ex exclus) qui ne possède pas déjà d'Outil Pokémon. Si le Pokémon auquel Sphère Protectrice est attachée est un Pokémon de base ou un Pokémon-ex, défaussez Sphère Protectrice.\n\nTant que Sphère Protectrice est attachée à un Pokémon, ce Pokémon n'a pas de Faiblesse.",
 		de: "As long as Protective Orb is attached to a Pokémon, that Pokémon has no Weakness."
 	},
