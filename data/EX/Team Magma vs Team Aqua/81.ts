303d473e0c6302e7b747d1c04824010f3e409be3
Phillip Gruneich <philgruneich@users.noreply.github.com>
2026-05-18 08:16:28 -0400

    patch: EN data EX/Team Magma vs Team Aqua (#1451)

--- data/EX/Team Magma vs Team Aqua/81.ts
+++ data/EX/Team Magma vs Team Aqua/81.ts
@@ -15,6 +15,7 @@ const card: Card = {
 	trainerType: "Tool",
 
 	effect: {
+		en: "Attach Team Magma Belt to 1 of your Pokémon with Team Magma in its name that doesn't already have a Pokémon Tool attached to it. If that Pokémon is Knocked Out, discard this card. At any time between turns, if the Pokémon Team Magma Belt is attached to is your Active Pokémon, search your deck for a card that evolves from that Pokémon and put it on that Pokémon. (This counts as evolving that Pokémon.) Shuffle your deck afterward, then discard Team Magma Belt.",
 		fr: "Attachez Raclée de Team Magma à 1 de vos Pokémon dont le nom comporte Team Magma et qui ne possède pas déjà d'Outil Pokémon. Si ce Pokémon est mis K.O., défaussez cette carte.\n\nN'importe quand entre deux tours, si le Pokémon auquel Raclée de Team Magma est attaché est votre Pokémon Actif, cherchez dans votre deck une carte qui évolue de ce Pokémon et placez-la dessus. (Vous faites ainsi évoluer ce Pokémon). Ensuite, mélangez votre deck et défaussez Raclée de Team Magma.",
 		de: "[Lege Team Magma Gürtel an eines deiner Pokémon mit \"Magma's\" im Namen an] Zu jedem Zeitpunkt zwischen den Zügen darfst du, falls das mit Team Magma Gürtel ausgerüstete Pokémon dein aktives Pokémon ist, darfst du eine Karte aus deinem Deck suchen, die sich aus dieser Karte entwickelt und lege sie auf das Pokémon. (Dies zählt als entwickeln des Pokémons.) Mische dein Deck danach, dann lege Team Magma Gürtel ab."
 	},
