5d0703cfeebdfa1ebc507395062a4f7a436306a4
Phil Gruneich <github@onetapless.com>
2026-05-18 20:51:42 -0400

    patch(data): missing EN attack data HGSS/Unleashed

--- data/HeartGold & SoulSilver/Unleashed/90.ts
+++ data/HeartGold & SoulSilver/Unleashed/90.ts
@@ -39,22 +39,26 @@ const card: Card = {
 
 	attacks: [{
 		name: {
+			en: "Detonation Spin",
 			de: "Explosionswirbel"
 		},
 
 		damage: 90,
 
 		effect: {
+			en: "Discard a Fire Energy attached to Entei & Raikou LEGEND.",
 			de: "Lege 1 an Entei & Raikou-LEGENDE angelegte -Energie auf deinen Ablagestapel."
 		},
 
 		cost: ["Fire", "Colorless"]
 	}, {
 		name: {
+			en: "Thunder Fall",
 			de: "Donnerfall"
 		},
 
 		effect: {
+			en: "Discard all Energy attached to Entei & Raikou LEGEND. This attack does 80 damage to each Pokémon that has any Poké-Powers (both yours and your opponent's). This attack's damage isn't affected by Weakness or Resistance.",
 			de: "Lege alle an Entei & Raikou-LEGENDE angelegte Energien auf deinen Ablagestapel. Dieser Angriff fügt jedem Pokémon (deinen und denen deines Gegners), das NICHT über Poké-Power verfügt, 80 Schadenspunkte zu. Der Schaden dieses Angriffs wird durch Schwäche und Resistenz nicht verändert."
 		},
 
--- data/HeartGold & SoulSilver/Unleashed/92.ts
+++ data/HeartGold & SoulSilver/Unleashed/92.ts
@@ -43,24 +43,28 @@ const card: Card = {
 
 	attacks: [{
 		name: {
+			en: "Thunderbolt Spear",
 			de: "Blitzspeer"
 		},
 
 		damage: 150,
 
 		effect: {
+			en: "Raikou & Suicune LEGEND does 50 damage to itself and don't apply Weakness to this damage.",
 			de: "Raikou & Suicune-LEGENDE fügt sich selbst 50 Schadenspunkte zu; wende dabei Schwäche nicht an."
 		},
 
 		cost: ["Lightning", "Lightning", "Colorless"]
 	}, {
 		name: {
+			en: "Aurora Gain",
 			de: "Aurora-Verstärkung"
 		},
 
 		damage: 50,
 
 		effect: {
+			en: "Remove 5 damage counters from Raikou & Suicune LEGEND.",
 			de: "Entferne 5 Schadensmarken von Raikou & Suicune-LEGENDE."
 		},
 
