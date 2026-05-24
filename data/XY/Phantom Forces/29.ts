ec737d3d8bc50b560c86cbda9edfbae3d5197eed
Phil Gruneich <github@onetapless.com>
2026-05-23 21:54:34 -0400

    patch(data): XY/Phantom Forces

--- data/XY/Phantom Forces/29.ts
+++ data/XY/Phantom Forces/29.ts
@@ -88,6 +88,10 @@ const card: Card = {
 
 	retreat: 1,
 
+	description: {
+		en: "They make their home in deserts. They can generate their energy from basking in the sun, so eating food is not a requirement.",
+	},
+
 	thirdParty: {
 		cardmarket: 281833,
 		tcgplayer: 94162
