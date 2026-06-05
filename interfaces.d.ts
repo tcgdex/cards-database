302e39795a02f2612f2e64ac1400f1501409340a
Jared Shillingburg <jshill103@gmail.com>
2026-06-05 16:11:47 -0600

    Add 'fernando-cifuentes' to the interface list
    add regular stamp and gold stamp 'fernando-cifuentes' variants

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -34,7 +34,8 @@ export type VariantStamps = '1st-edition' | 'w-promo' | 'pre-release' | 'pokemon
 	| 'zachary-bokhari' | 'shuto-itagaki' | 'snowflake' | 'trick-or-trade' | 'horizons' | 'gamestop' | 'eb-games'
 	| 'illustration-contest-2024' | 'worlds-2025' | 'top-eight' | "champion" | "master-ball-league" | "ultra-ball-league" | "judge" | "asia-promo"
 	| "international-championship-europe" | "international-championship-latin-america" | "international-championship-north-america" | 'ace-trainer'
-	| 'pikachu' | 'bulbasaur' | 'squirtle' | 'charmander' | 'pokeball' | '30th-pokeday' | 'mcdonalds' | 'pokemon-together'
+	| 'pikachu' | 'bulbasaur' | 'squirtle' | 'charmander' | 'pokeball' | '30th-pokeday' | 'mcdonalds' | 'pokemon-together' | 'fernando-cifuentes'
+	| 'fernando-cifuentes-gold'
 
 export interface variant_detailed {
 	/**
