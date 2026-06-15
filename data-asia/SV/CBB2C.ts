5643976faf9794b8617d5951f4bfe583d807914a
advra <adriandalonzo@gmail.com>
2026-06-15 02:11:41 -0700

    patch(data): zh-cn data simplified chinese gem pack 1 and 5 set definitions (#1627)
    * Initial definiton of gem pack vol 1 to 5
    
    * Removed en
    
    ---------
    
    Co-authored-by: advra <aalonzo33@gatech.edu>

---
+++ data-asia/SV/CBB2C.ts
@@ -0,0 +1,18 @@
+import { Set } from '../../interfaces'
+import serie from '../SV'
+
+const set: Set = {
+	id: 'CBB2C',
+	name: {
+		'zh-cn': '宝石包Vol.2',
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 15
+	},
+	releaseDate: '2025-05-16',
+}
+
+export default set
