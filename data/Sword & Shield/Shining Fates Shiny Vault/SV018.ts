c54ace6f637d03b38177220c64beb5c416f33009
Ryan <112320693+FalconChipp@users.noreply.github.com>
2026-07-05 10:54:32 +0100

    (data): Split Shining Fates Shiny Vault into own set and added pricing IDs (#1666)
    * Split SHF Shiny vault out of main set
    
    * Update Shining Fates Shiny Vault.ts
    
    * Add pricing IDs
    
    * Remove colon from parent to match folder name
    
    Fixes build fail
    
    * Restore Silver Tempest Trainer Gallery accidentally dropped in master merge
    
    ---------
    
    Co-authored-by: Thomas <91132581+thomas-bassett@users.noreply.github.com>

---
+++ data/Sword & Shield/Shining Fates Shiny Vault/SV018.ts
@@ -0,0 +1,73 @@
+import { Card } from '../../../interfaces'
+import Set from '../Shining Fates Shiny Vault'
+
+const card: Card = {
+	dexId: [850],
+	set: Set,
+
+	name: {
+		fr: "Grillepattes",
+		en: "Sizzlipede",
+		es: "Sizzlipede",
+		it: "Sizzlipede",
+		pt: "Sizzlipede",
+		de: "Thermopod"
+	},
+
+	illustrator: "Shigenori Negishi",
+	rarity: "Shiny rare",
+	category: "Pokemon",
+	hp: 70,
+	types: ["Fire"],
+
+	attacks: [{
+		name: {
+			fr: "Morsure",
+			en: "Bite",
+			es: "Mordisco",
+			it: "Morso",
+			pt: "Mordida",
+			de: "Biss"
+		},
+
+		damage: 10,
+		cost: ["Colorless"]
+	}, {
+		name: {
+			fr: "Fournaise",
+			en: "Combustion",
+			es: "Combustión",
+			it: "Fuoco Continuo",
+			pt: "Combustão",
+			de: "Glühen"
+		},
+
+		damage: 50,
+		cost: ["Fire", "Colorless", "Colorless"]
+	}],
+
+	weaknesses: [{
+		type: "Water",
+		value: "×2"
+	}],
+
+	retreat: 1,
+	regulationMark: "D",
+	stage: "Basic",
+
+	description: {
+		en: "It stores flammable gas in its body and uses it to generate heat. The yellow sections on its belly get particularly hot."
+	},
+
+	variants: [
+		{
+			type: 'holo',
+			thirdParty: {
+				cardmarket: 539478,
+				tcgplayer: 232366
+			}
+		},
+	],
+}
+
+export default card
---
+++ data/Sword & Shield/Shining Fates Shiny Vault/SV025.ts
@@ -0,0 +1,61 @@
+import { Card } from '../../../interfaces'
+import Set from '../Shining Fates Shiny Vault'
+
+const card: Card = {
+	dexId: [816],
+	set: Set,
+
+	name: {
+		fr: "Larméléon",
+		en: "Sobble",
+		es: "Sobble",
+		it: "Sobble",
+		pt: "Sobble",
+		de: "Memmeon"
+	},
+
+	illustrator: "Saya Tsuruta",
+	rarity: "Shiny rare",
+	category: "Pokemon",
+	hp: 60,
+	types: ["Water"],
+
+	attacks: [{
+		name: {
+			fr: "Pistolet à O",
+			en: "Water Gun",
+			es: "Pistola Agua",
+			it: "Pistolacqua",
+			pt: "Revólver d'Água",
+			de: "Aquaknarre"
+		},
+
+		damage: 20,
+		cost: ["Water"]
+	}],
+
+	weaknesses: [{
+		type: "Lightning",
+		value: "×2"
+	}],
+
+	retreat: 1,
+	regulationMark: "D",
+	stage: "Basic",
+
+	description: {
+		en: "When scared, this Pokémon cries. Its tears pack the chemical punch of 100 onions, and attackers won't be able to resist weeping."
+	},
+
+	variants: [
+		{
+			type: 'holo',
+			thirdParty: {
+				cardmarket: 539513,
+				tcgplayer: 232375
+			}
+		},
+	],
+}
+
+export default card
