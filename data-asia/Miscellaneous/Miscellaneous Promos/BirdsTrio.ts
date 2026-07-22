e1f1ad6c6319226bc046229d0cb8af243097400e
advra <aalonzo33@gatech.edu>
2026-07-22 11:23:50 -0700

    Added en and ja birds trio jumbo card

--- data-asia/Miscellaneous/Miscellaneous Promos/BirdsTrio.ts
+++ data-asia/Miscellaneous/Miscellaneous Promos/BirdsTrio.ts
@@ -3,7 +3,7 @@ import Set from "../Miscellaneous Promos"
 
 const card: Card = {
   name: {
-    en: "Articuno, Moltres, and Zapdos",
+    ja: "サンダーとフリーザーとファイヤー",
   },
   illustrator: "Mitsuhiro Arita",
   rarity: "Promo",
@@ -19,15 +19,15 @@ const card: Card = {
   attacks: [
     {
       name: {
-        en: "Big Bang",
+        ja: "ビッグバーン",
       },
       cost: [
-        "Fire",
         "Lightning",
-        "Water"
+        "Water",
+        "Fire",
       ],
       effect: {
-        en: "Shuffle Articuno, Molres and Zapdos into your deck."
+        ja: "このワザを使ったら、このカードを山札にもどし、その山札をよく切る。そのとき、このカードについていたダメージカウンターを全部取りのぞき、ついていたカードも全てトラッシュする。"
       },
       damage: "200",
     },
@@ -36,7 +36,7 @@ const card: Card = {
   retreat: 0,
 
   description: {
-    en: "These legendary bird Pokémon are above electricity, fire, water, and all other elements."
+    ja: "雷、氷、炎とそれぞれの属性の頂点にたつと言われる伝説の鳥ポケモンだ。"
   },
 
   variants: [
@@ -44,7 +44,7 @@ const card: Card = {
       type: 'normal',
       size: 'jumbo',
       thirdParty: {
-        tcgplayer: 211417
+        tcgplayer: 617213
       }
     },
   ]
