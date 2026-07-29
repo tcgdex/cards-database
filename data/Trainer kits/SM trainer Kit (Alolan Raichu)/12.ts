3d692730795bc247edb7c8fab708b01a154bdcce
advra <aalonzo33@gatech.edu>
2026-07-29 03:03:15 -0700

    added cards initial

---
+++ data/Trainer kits/SM trainer Kit (Alolan Raichu)/12.ts
@@ -0,0 +1,28 @@
+import { Card } from '../../../interfaces'
+import Set from '../SM trainer Kit (Alolan Raichu)'
+
+const card: Card = {
+	name: {
+		en: "Lightning Energy",
+	},
+
+	rarity: "None",
+	category: "Energy",
+	set: Set,
+	retreat: 0,
+
+	thirdParty: {
+		tcgplayer: 192937,
+	},
+
+	variants: [
+		{
+			type: "normal",
+			thirdParty: {
+				tcgplayer: 192937,
+			},
+		},
+	],
+}
+
+export default card
---
+++ data/Trainer kits/SM trainer Kit (Alolan Raichu)/24.ts
@@ -0,0 +1,31 @@
+import { Card } from '../../../interfaces'
+import Set from '../SM trainer Kit (Alolan Raichu)'
+
+const card: Card = {
+	name: {
+		en: "Psychic Energy",
+	},
+
+	rarity: "None",
+	category: "Energy",
+	set: Set,
+	types: [
+		"Psychic",
+	],
+	retreat: 0,
+
+	thirdParty: {
+		tcgplayer: 152859,
+	},
+
+	variants: [
+		{
+			type: "normal",
+			thirdParty: {
+				tcgplayer: 152859,
+			},
+		},
+	],
+}
+
+export default card
