6d28d540b89f6a74cdc28dd15983a917f8c22328
advra <aalonzo33@gatech.edu>
2026-08-06 09:55:30 -0700

    Corrected set import

--- data-asia/S/SC1a/007.ts
+++ data-asia/S/SC1a/007.ts
@@ -1,4 +1,4 @@
-import Set from "../Sc1a"
+import Set from "../SC1a"
 import { Card } from "../../../interfaces"
 
 const card: Card = {
--- data-asia/S/SC1a/009.ts
+++ data-asia/S/SC1a/009.ts
@@ -1,5 +1,5 @@
 import { Card } from "../../../interfaces"
-import Set from "../Sc1a"
+import Set from "../SC1a"
 
 const card: Card = {
 	set: Set,
