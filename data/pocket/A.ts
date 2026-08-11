48659b03734fbf6912d7c5806b969c3b8c35e425
Avior <github@avior.me>
2026-08-11 12:20:10 +0200

    chore: move files to new location

---
+++ data/pocket/A.ts
@@ -0,0 +1,17 @@
+import { Serie } from 'models/serie'
+
+const serie: Serie = {
+	id: "A",
+	name: {
+		'en-en': "A",
+		'de-de': "A",
+		'es-es': "A",
+		'fr-fr': "A",
+		'it-it': "A",
+		'pt-br': "A",
+		'ko-ko': "A",
+		'ja-ja': 'A'
+	},
+}
+
+export default serie
---
+++ data/pocket/B.ts
@@ -0,0 +1,17 @@
+import { Serie } from 'models/serie'
+
+const serie: Serie = {
+	id: "B",
+	name: {
+		'en-en': "B",
+		'de-de': "B",
+		'es-es': "B",
+		'fr-fr': "B",
+		'it-it': "B",
+		'pt-br': "B",
+		'ko-ko': "B",
+		'ja-ja': 'B'
+	},
+}
+
+export default serie
